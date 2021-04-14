98aa379a5b66d3c911eca5f9436c1723317883bb
Joshua Peek <josh@joshpeek.com>
2021-04-13 15:08:04 -1000

    Add sparql test

---
+++ test_sparql.py
@@ -0,0 +1,16 @@
+from sparql import sparql
+
+
+def test_sparql():
+    results = sparql(
+        """
+        SELECT ?item ?itemLabel WHERE {
+          ?item wdt:P31 wd:Q146.
+          SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". }
+        }
+        LIMIT 10
+        """
+    )
+    assert len(results) == 10
+    assert results[0]["item"].startswith("Q")
+    assert results[0]["itemLabel"]
