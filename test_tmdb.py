8764e4a6530b4ce54db9b534cbb1395e86b2e4fa
Joshua Peek <josh@joshpeek.com>
2021-04-23 13:21:47 -0700

    Test non matches

--- test_tmdb.py
+++ test_tmdb.py
@@ -6,6 +6,8 @@ def test_movie():
     assert movie
     assert movie["title"] == "The Shawshank Redemption"
 
+    assert not tmdb.movie("1000000000")
+
 
 def test_find():
     results = tmdb.find("tt0111161", "imdb_id")
@@ -13,7 +15,12 @@ def test_find():
     assert results["movie_results"]
     assert results["movie_results"][0]["title"] == "The Shawshank Redemption"
 
+    results = tmdb.find("tt10000000000", "imdb_id")
+    assert not results["movie_results"]
+
 
 def test_find_by_imdb_id():
     tmdb_id = tmdb.find_by_imdb_id("tt0111161", type="movie")
     assert tmdb_id == "278"
+
+    assert not tmdb.find_by_imdb_id("tt10000000000", type="movie")
