fbcf92c557a6f4af91fb17c7797eb2e719bd0a3c
Joshua Peek <josh@joshpeek.com>
2021-05-11 16:31:29 -0700

    Link directly to issue statements

--- P345_report.py
+++ P345_report.py
@@ -6,12 +6,15 @@ from sparql import sparql
 
 def main():
     query = """
-    SELECT ?item ?imdb WHERE {
+    SELECT ?statement ?value WHERE {
       SERVICE bd:sample {
-        ?item wdt:P345 ?imdb.
-        bd:serviceParam bd:sample.limit 500 .
-        bd:serviceParam bd:sample.sampleType "RANDOM".
+        ?item p:P345 ?statement.
+        bd:serviceParam bd:sample.limit 5 ;
+          bd:sample.sampleType "RANDOM".
       }
+      ?statement wikibase:rank ?rank.
+      FILTER(?rank != wikibase:DeprecatedRank)
+      ?statement ps:P345 ?value.
     }
     """
     results = sparql(query)
@@ -21,38 +24,38 @@ def main():
     imdb_link_unknown = []
 
     for result in tqdm(results):
-        id = result["imdb"]
+        id = result["value"]
 
         if imdb.is_valid_id(id):
             new_id = imdb.canonical_id(id)
 
             if new_id is None:
-                imdb_link_rot.append((result["item"], id))
+                imdb_link_rot.append((result["statement"], id))
             elif id is not new_id:
-                imdb_redirects.append((result["item"], id, new_id))
+                imdb_redirects.append((result["statement"], id, new_id))
 
         else:
-            imdb_link_unknown.append((result["item"], id))
+            imdb_link_unknown.append((result["statement"], id))
 
     imdb_link_rot.sort()
     imdb_redirects.sort()
     imdb_link_unknown.sort()
 
     print("== IMDb link rot ==")
-    for (qid, imdb_id) in imdb_link_rot:
+    for (statement, imdb_id) in imdb_link_rot:
         print(
             "* "
-            + wiki_qid(qid)
+            + wiki_statement(statement)
             + ": "
             + wiki_link(imdb_id, imdb.formatted_url(imdb_id))
         )
     print("")
 
     print("== IMDb redirects ==")
-    for (qid, imdb_id, imdb_canonical_id) in imdb_redirects:
+    for (statement, imdb_id, imdb_canonical_id) in imdb_redirects:
         print(
             "* "
-            + wiki_qid(qid)
+            + wiki_statement(statement)
             + ": "
             + wiki_link(imdb_id, imdb.formatted_url(imdb_id))
             + " → "
@@ -61,8 +64,8 @@ def main():
     print("")
 
     print("== IMDb unknown IDs ==")
-    for (qid, imdb_id) in imdb_link_unknown:
-        print("* " + wiki_qid(qid) + ": " + imdb_id)
+    for (statement, imdb_id) in imdb_link_unknown:
+        print("* " + wiki_statement(statement) + ": " + imdb_id)
     print("")
 
 
@@ -70,8 +73,12 @@ def wiki_link(title, url):
     return "[{url} {title}]".format(url=url, title=title)
 
 
-def wiki_qid(qid):
-    return "{{Q|" + qid.replace("Q", "") + "}}"
+def wiki_statement(statement):
+    statement = statement.replace("$", "-")
+    return wiki_link(
+        "wds:{}".format(statement),
+        "http://www.wikidata.org/entity/statement/{}".format(statement),
+    )
 
 
 if __name__ == "__main__":
