aa10725258d9c5ccdbe653763263519d11030439
Joshua Peek <josh@joshpeek.com>
2021-05-17 12:59:42 -0700

    Add P9586 report

---
+++ P9586_report.py
@@ -0,0 +1,84 @@
+from tqdm import tqdm
+
+import appletv
+import sparql
+import wikitext
+
+
+def main():
+    qids = sparql.sample_items("P9586", limit=100) | sparql.recent_items(
+        "P9586", limit=100
+    )
+
+    results = sparql.fetch_statements(qids, ["P6398", "P9586"])
+
+    link_rot = []
+    itunes_statements = []
+    itunes_diff = []
+
+    for qid in tqdm(results):
+        item = results[qid]
+
+        appletv_movie = None
+        for (statement, value) in item["P9586"]:
+            appletv_movie = appletv.movie(value)
+            if not appletv_movie:
+                link_rot.append((statement, value))
+
+        itunes_ids = set()
+        for (statement, value) in item.get("P6398", []):
+            itunes_ids.add(int(value))
+
+        if not appletv_movie:
+            continue
+
+        if len(itunes_ids) == 0 and appletv_movie["itunes_id"]:
+            itunes_statements.append((qid, appletv_movie["itunes_id"]))
+        elif (
+            len(itunes_ids) > 0
+            and appletv_movie["itunes_id"]
+            and appletv_movie["itunes_id"] not in itunes_ids
+        ):
+            itunes_diff.append((qid, list(itunes_ids)[0], appletv_movie["itunes_id"]))
+
+    link_rot.sort()
+    itunes_statements.sort()
+    itunes_diff.sort()
+
+    print("== Link rot ==")
+    for (statement, id) in link_rot:
+        print("* " + wikitext.statement(statement) + ": " + wiki_appletv_link(id))
+    print("")
+
+    print("== iTunes Store statements ==")
+    for (qid, value) in itunes_statements:
+        print("{{Statement|" + qid + '|P6398|"' + str(value) + '"}}')
+    print("")
+
+    print("== iTunes Store differences ==")
+    for (qid, actual_itunes_id, expected_itunes_id) in itunes_diff:
+        print(
+            "* "
+            + wikitext.item(qid)
+            + ": "
+            + wiki_itunes_link(actual_itunes_id)
+            + " vs "
+            + wiki_itunes_link(expected_itunes_id)
+        )
+    print("")
+
+
+def wiki_appletv_link(appletv_id):
+    return wikitext.link(
+        appletv_id, "https://tv.apple.com/us/movie/{}".format(appletv_id)
+    )
+
+
+def wiki_itunes_link(itunes_id):
+    return wikitext.link(
+        itunes_id, "http://itunes.apple.com/us/movie/id{}".format(itunes_id)
+    )
+
+
+if __name__ == "__main__":
+    main()
---
+++ appletv.py
@@ -0,0 +1,81 @@
+import json
+import re
+
+import requests
+from bs4 import BeautifulSoup
+
+
+def movie(id):
+    soup = fetch("https://tv.apple.com/us/movie/{}".format(id))
+    if not soup:
+        return None
+
+    return {
+        "id": id,
+        "itunes_id": extract_itunes_id(soup),
+    }
+
+
+request_headers = {
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+    "Accept-Language": "en-us",
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) "
+    + "AppleWebKit/605.1.15 (KHTML, like Gecko) "
+    + "Version/14.1.1 Safari/605.1.15",
+}
+
+
+def fetch(url):
+    r = requests.get(url, headers=request_headers)
+    r.raise_for_status()
+
+    html = r.text
+    soup = BeautifulSoup(html, "html.parser")
+
+    if soup.find("h1", text="This content is no longer available."):
+        return None
+
+    link = soup.find("link", attrs={"rel": "canonical"})
+    if not link:
+        return None
+
+    return soup
+
+
+def extract_shoebox(soup):
+    script = soup.find("script", {"type": "fastboot/shoebox", "id": "shoebox-uts-api"})
+    if not script:
+        return []
+
+    boxes = []
+    data = json.loads(script.string)
+    for key in data:
+        subdata = json.loads(data[key])
+        if "d" in subdata and "data" in subdata["d"]:
+            boxes.append(subdata["d"]["data"])
+    return boxes
+
+
+def extract_itunes_id(soup):
+    for data in extract_shoebox(soup):
+        if "content" in data and "playables" in data["content"]:
+            for playable in data["content"]["playables"]:
+                if playable.get("isItunes", False) is True:
+                    return int(playable["externalId"])
+
+        if "howToWatch" in data:
+            for way in data["howToWatch"]:
+                if way["channelId"] != "tvs.sbd.9001":
+                    continue
+
+                if not way.get("punchoutUrls"):
+                    continue
+
+                m = re.match(
+                    r"itmss://itunes.apple.com/us/[^/]+/[^/]+/id(\d+)",
+                    way["punchoutUrls"]["open"],
+                )
+                if m:
+                    return int(m.group(1))
+
+    return None
