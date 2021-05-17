7d767c504640acc85a5d9a5c986a2c1007aa6eac
Joshua Peek <josh@joshpeek.com>
2021-05-17 15:29:31 -0700

    Modernize itunes reports

--- P6398_report.py
+++ P6398_report.py
@@ -1,37 +1,36 @@
 import itunes
+import sparql
 import wikitext
-from sparql import sparql
+from page_extract import page_qids
 
 
 def main():
-    query = """
-    SELECT ?statement ?value WHERE {
-      SERVICE bd:sample {
-        ?item p:P6398 ?statement.
-        bd:serviceParam bd:sample.limit 5000 ;
-          bd:sample.sampleType "RANDOM".
-      }
-      ?statement wikibase:rank ?rank.
-      FILTER(?rank != wikibase:DeprecatedRank)
-      ?statement ps:P6398 ?value.
-    }
-    """
-    results = sparql(query)
+    qids = (
+        sparql.sample_items("P6398", type="random", limit=1000)
+        | sparql.sample_items("P6398", type="created", limit=1000)
+        | page_qids("User:Josh404Bot/Maintenance_reports/P6398")
+    )
+
+    results = sparql.fetch_statements(qids, ["P6398"])
 
     itunes_ids = set()
     itunes_id_statement = {}
 
-    for result in results:
-        id = int(result["value"])
-        itunes_ids.add(id)
-        itunes_id_statement[id] = result["statement"]
+    for qid in results:
+        item = results[qid]
+
+        if "P6398" not in item:
+            continue
+
+        for (statement, value) in item["P6398"]:
+            id = int(value)
+            itunes_ids.add(id)
+            itunes_id_statement[id] = statement
 
     itunes_link_rot = []
 
     for (id, obj) in itunes.batch_lookup(itunes_ids):
-        if obj:
-            pass
-        else:
+        if not obj:
             itunes_link_rot.append((itunes_id_statement[id], id))
 
     itunes_link_rot.sort()
@@ -43,7 +42,8 @@ def main():
             + wikitext.statement(statement)
             + ": "
             + wikitext.link(
-                itunes_id, "https://itunes.apple.com/us/movie/id{}".format(itunes_id)
+                itunes_id,
+                "https://itunes.apple.com/us/movie/id{}".format(itunes_id),
             )
         )
     print("")
