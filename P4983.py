68ae97e2e16cb05f311410549b0ebc77a5bdae79
Joshua Peek <josh@joshpeek.com>
2021-05-26 20:36:53 -0700

    Fix tmdb property id

--- P4983.py
+++ P4983.py
@@ -31,7 +31,7 @@ def main():
       OPTIONAL { ?item wdt:P4835 ?tvdb. }
 
       # Exclude items that already have a TMDB TV ID
-      OPTIONAL { ?item wdt:P4985 ?tmdb. }
+      OPTIONAL { ?item wdt:P4983 ?tmdb. }
       FILTER(!(BOUND(?tmdb)))
 
       # Generate random sorting key
