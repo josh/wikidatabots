998bc3c4c7b53f447364a71f2c668410707457cf
Joshua Peek <josh@joshpeek.com>
2021-05-28 15:42:10 -0700

    Add freebase references to P4985

---
+++ P4985_freebase_reference.py
@@ -0,0 +1,49 @@
+from tqdm import tqdm
+
+import tmdb
+from quickstatements import today
+from sparql import sparql
+
+
+def main():
+    """
+    Find Wikidata items have a TMDb person ID (P4985) without references.
+    Attempt to verify the ID by looking it up by the Freebase MID via the TMDb API.
+    If there's a match, create a reference.
+
+    Outputs QuickStatements CSV commands.
+    """
+
+    query = """
+    SELECT DISTINCT ?item ?statement ?freebase ?tmdb WHERE {
+      ?item wdt:P646 ?freebase.
+      ?item p:P4985 ?statement.
+      ?statement ps:P4985 ?tmdb.
+      OPTIONAL {
+        ?statement prov:wasDerivedFrom ?reference.
+        ?reference pr:P248 wd:Q20828898;
+                   pr:P646 [].
+      }
+      FILTER(!BOUND(?reference))
+    }
+    LIMIT 2500
+    """
+    results = sparql(query)
+
+    print("qid,P4985,S248,s646,s813")
+    for result in tqdm(results):
+        person = tmdb.find(id=result["freebase"], source="freebase_mid", type="person")
+        if person and person["id"] == int(result["tmdb"]):
+            print(
+                '{},"""{}""",{},"""{}""",{}'.format(
+                    result["item"],
+                    person["id"],
+                    "Q20828898",
+                    result["freebase"],
+                    today(),
+                )
+            )
+
+
+if __name__ == "__main__":
+    main()
