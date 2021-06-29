59096ec55b5a78776bf503243c11f22b8405e8ec
Joshua Peek <josh@joshpeek.com>
2021-06-29 13:53:01 -0700

    Drop suggestions from report

--- report_P9586.py
+++ report_P9586.py
@@ -15,7 +15,6 @@ def main():
     results = sparql.fetch_statements(qids, ["P6398", "P9586"])
 
     not_found = []
-    itunes_statements = []
     itunes_diff = []
 
     for qid in tqdm(results):
@@ -35,9 +34,7 @@ def main():
             continue
 
         if len(itunes_ids) == 0 and appletv_movie["itunes_id"]:
-            itunes_statements.append(
-                (qid, "P6398", '"{}"'.format(appletv_movie["itunes_id"]))
-            )
+            pass
         elif (
             len(itunes_ids) > 0
             and appletv_movie["itunes_id"]
@@ -46,7 +43,6 @@ def main():
             itunes_diff.append((qid, list(itunes_ids)[0], appletv_movie["itunes_id"]))
 
     not_found.sort()
-    itunes_statements.sort()
     itunes_diff.sort()
 
     print("== Not found ==")
@@ -58,10 +54,6 @@ def main():
             + wikitext.external_id(id, P9586_URL_FORMATTER)
         )
     print("")
-
-    print(
-        wikitext.statements_section("iTunes Store suggestions", uniq(itunes_statements))
-    )
 
     print("== iTunes Store differences ==")
     for (qid, actual_itunes_id, expected_itunes_id) in uniq(itunes_diff):
