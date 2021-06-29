b1c4f24751f8af00a3bd6bb9fb2ef14983f3844c
Joshua Peek <josh@joshpeek.com>
2021-06-28 17:02:46 -0700

    Bump count

--- not_found_P4985.py
+++ not_found_P4985.py
@@ -4,7 +4,7 @@ from report_utils import sample_qids
 
 
 def main():
-    qids = sample_qids("P4985", count=500)
+    qids = sample_qids("P4985", count=2500)
     results = sparql.fetch_statements(qids, ["P4985"])
 
     for qid in results:
