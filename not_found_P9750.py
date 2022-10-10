b6df1fd79c87528e99acdf93748367bfbcdc8231
Joshua Peek <josh@users.noreply.github.com>
2022-10-10 21:50:20 +0000

    Extract APPLE_TV_EPISODE_ID_PID constant

--- not_found_P9750.py
+++ not_found_P9750.py
@@ -4,6 +4,7 @@ import appletv
 import sparql
 from items import WITHDRAWN_IDENTIFIER_VALUE_QID
 from page import page_qids
+from properties import APPLE_TV_EPISODE_ID_PID
 from sparql import sample_items
 
 MATCHER = re.compile("^umc.cmc.[a-z0-9]{22,25}$")
@@ -14,15 +15,15 @@ def main():
         type="episode", id="umc.cmc.1488ez4dp942etebq3p85k1np"
     )
 
-    qids = sample_items("P9750", limit=250)
+    qids = sample_items(APPLE_TV_EPISODE_ID_PID, limit=250)
     qids |= page_qids("Wikidata:Database reports/Constraint violations/P9750")
 
-    results = sparql.fetch_statements(qids, ["P9750"])
+    results = sparql.fetch_statements(qids, [APPLE_TV_EPISODE_ID_PID])
 
     for qid in results:
         item = results[qid]
 
-        for (statement, value) in item.get("P9750", []):
+        for (statement, value) in item.get(APPLE_TV_EPISODE_ID_PID, []):
             if not MATCHER.match(value):
                 continue
 
