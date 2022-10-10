c59de59902b5295af84b721c9ae0fd014f7ccbae
Joshua Peek <josh@users.noreply.github.com>
2022-10-10 20:17:04 +0000

    Allow test to be skipped

--- test_opencritic.py
+++ test_opencritic.py
@@ -1,6 +1,9 @@
+import pytest
+
 import opencritic
 
 
+@pytest.mark.skipif(opencritic.RAPIDAPI_KEY is None, reason="Missing RAPIDAPI_KEY")
 def test_fetch_game():
     game = opencritic.fetch_game(1548)
     assert game
