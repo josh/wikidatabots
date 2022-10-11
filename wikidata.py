d36c02fa62eaf2f0af397f39ef6e35b9dbcc961c
Joshua Peek <josh@users.noreply.github.com>
2022-10-11 23:05:57 +0000

    Persist password file

--- wikidata.py
+++ wikidata.py
@@ -1,3 +1,11 @@
+import os
+
+import pywikibot.config
 from pywikibot import Site
 
 SITE = Site("wikidata", "wikidata")
+
+if "WIKIDATA_USERNAME" in os.environ:
+    pywikibot.config.usernames["wikidata"]["wikidata"] = os.environ["WIKIDATA_USERNAME"]
+
+pywikibot.config.password_file = "user-password.py"
