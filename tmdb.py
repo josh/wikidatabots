969e560df4039cfe0bf85fb577313c058168f960
Joshua Peek <josh@users.noreply.github.com>
2022-10-14 16:06:16 +0000

    Revert "Retry"
    This reverts commit 9fabf3199b5148c41f590ab447e41931c773b6d6.

--- tmdb.py
+++ tmdb.py
@@ -97,7 +97,6 @@ find_types: set[FindType] = set(["movie", "person", "tv", "tv_episode", "tv_seas
 FindResult = dict[str, Any]
 
 
-@backoff.on_exception(backoff.expo, requests.exceptions.HTTPError, max_tries=3)
 def find(
     id: str | int,
     source: FindSource,
