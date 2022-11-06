812bb955449cfe4383019a4f2bb1860f04cc3ba0
Joshua Peek <josh@users.noreply.github.com>
2022-11-06 22:01:16 +0000

    Remove page statements script

--- page.py
+++ page.py
@@ -6,27 +6,8 @@ MUST be logged in first. See pwb.py
 
 import logging
 import re
-import sys
 
-import pywikibot
 import requests
-
-
-def edit(title: str, text: str, username: str, summary: str | None = None):
-    """
-    Edit an existing wiki page.
-    """
-
-    import pywikibot
-    import pywikibot.config
-
-    pywikibot.config.usernames["wikidata"]["wikidata"] = username
-    pywikibot.config.password_file = "user-password.py"
-    site = pywikibot.Site("wikidata", "wikidata")
-    page = pywikibot.Page(site, title)
-    page.text = text
-    page.save(summary)
-    return page
 
 
 def page_text(page_title: str) -> str | None:
@@ -81,34 +62,3 @@ def blocked_qids() -> set[str]:
     if not _blocked_qids:
         _blocked_qids = page_qids("User:Josh404Bot/Blocklist", blocked=True)
     return _blocked_qids
-
-
-def page_statements(page_title: str) -> list[tuple[str, str, str]]:
-    text = page_text(page_title)
-    if not text:
-        logging.warn(f"page: {page_title} not found")
-        return []
-
-    return re.findall(r".* \((Q\d+)\) .* \((P\d+)\) \"([^\"]+)\"", text)
-
-
-if __name__ == "__main__":
-    import argparse
-    import os
-
-    parser = argparse.ArgumentParser(description="Create and edit Wikidata pages.")
-    parser.add_argument("--username", action="store")
-    parser.add_argument("--title", action="store")
-    parser.add_argument("--summary", action="store")
-    parser.add_argument("cmd", action="store")
-    args = parser.parse_args()
-
-    if args.cmd == "edit":
-        edit(
-            username=args.username or os.environ["WIKIDATA_USERNAME"],
-            title=args.title,
-            text=sys.stdin.read(),
-            summary=args.summary,
-        )
-    else:
-        parser.print_usage()
