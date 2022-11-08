27dae9bba7759dccc5e73112ccb418f4117e05e0
Joshua Peek <josh@users.noreply.github.com>
2022-11-08 22:04:18 +0000

    Add smoke test

---
+++ test_rdf_update.py
@@ -0,0 +1,14 @@
+from io import StringIO
+
+from rdf_update import process_graph
+
+username = "Test"
+
+
+def test_process_graph():
+    rdf = """
+    <http://www.wikidata.org/entity/statement/Q172241-6B571F20-7732-47E1-86B2-1DFA6D0A15F5>
+    <http://wikiba.se/ontology#rank>
+    <http://wikiba.se/ontology#DeprecatedRank> .
+    """
+    process_graph(username, StringIO(rdf), save=False)
