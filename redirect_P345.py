2f75b3e4e564510f51a4d6ee5cd6791a5fe1b0f1
Joshua Peek <josh@users.noreply.github.com>
2022-11-10 07:06:37 +0000

    Fix qid

--- redirect_P345.py
+++ redirect_P345.py
@@ -2,11 +2,35 @@ import logging
 
 import imdb
 import sparql
+import wikidata
 from constants import IMDB_ID_PID, REASON_FOR_DEPRECATED_RANK_PID, REDIRECT_QID
 
 
+def imdb_qids_missing_tmdb() -> set[wikidata.QID]:
+    query = """
+    SELECT DISTINCT ?item WHERE {
+        ?item wdt:P345 ?imdb.
+
+        OPTIONAL { ?item wdt:P4947 ?tmdb_movie_id. }
+        OPTIONAL { ?item wdt:P4983 ?tmdb_tv_id. }
+        OPTIONAL { ?item wdt:P4985 ?tmdb_person_id. }
+
+        FILTER(!(BOUND(?tmdb_movie_id)))
+        FILTER(!(BOUND(?tmdb_tv_id)))
+        FILTER(!(BOUND(?tmdb_person_id)))
+
+        BIND(MD5(CONCAT(STR(?item), STR(RAND()))) AS ?random)
+    }
+    ORDER BY ?random
+    LIMIT 100
+    """
+    results = sparql.sparql(query)
+    return set([result["item"] for result in results])
+
+
 def main():
-    qids = sparql.sample_items(IMDB_ID_PID, limit=100)
+    # qids = sparql.sample_items(IMDB_ID_PID, limit=100)
+    qids = imdb_qids_missing_tmdb()
     results = sparql.fetch_statements(qids, [IMDB_ID_PID])
 
     edit_summary = "Add claim for canonical IMDb ID"
@@ -27,8 +51,7 @@ def main():
 
             if id is not new_id:
                 print(
-                    f"wd:Q2851520 p:P345 "
-                    f'[ ps:P345 "{new_id}" ] ; '
+                    f'wd:{qid} wdt:P345 "{new_id}" ; '
                     f'wikidatabots:editSummary "{edit_summary}" . '
                 )
                 print(
