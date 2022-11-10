e9e45272b794ab5c6c07af79abf985823606df0c
Joshua Peek <josh@users.noreply.github.com>
2022-11-10 22:05:04 +0000

    Run P4983 as rdf patch

--- P4983.py
+++ P4983.py
@@ -4,6 +4,7 @@ import logging
 from typing import TypedDict
 
 import tmdb
+from constants import TMDB_TV_SERIES_ID_PID
 from page import blocked_qids
 from sparql import sparql
 from timeout import iter_until_deadline
@@ -16,13 +17,17 @@ class Item(TypedDict):
     tvdb: set[str]
 
 
+IMDB_ID_PROPERTY_NAME = "IMDb ID"
+TVDB_ID_PROPERTY_NAME = "TheTVDB.com series ID"
+
+
 def main():
     """
     Find Wikidata items that are missing a TMDb TV series ID (P4983) but have a
     IMDb ID (P345) or TheTVDB.com series ID (P4835). Attempt to look up the
     TV show via the TMDb API. If there's a match, create a new statement.
 
-    Outputs QuickStatements CSV commands.
+    Outputs RDF statements.
     """
 
     query = """
@@ -72,23 +77,31 @@ def main():
         if result["tvdb"]:
             item["tvdb"].add(result["tvdb"])
 
-    print("qid,P4983")
     for qid in iter_until_deadline(items):
         item = items[qid]
         tmdb_ids: set[int] = set()
+        tmdb_id_sources: dict[int, str] = {}
 
         for imdb_id in item["imdb"]:
             tv = tmdb.find(id=imdb_id, source="imdb_id", type="tv")
             if tv:
                 tmdb_ids.add(tv["id"])
+                tmdb_id_sources.setdefault(tv["id"], IMDB_ID_PROPERTY_NAME)
 
         for tvdb_id in item["tvdb"]:
             tv = tmdb.find(id=tvdb_id, source="tvdb_id", type="tv")
             if tv:
                 tmdb_ids.add(tv["id"])
+                tmdb_id_sources.setdefault(tv["id"], TVDB_ID_PROPERTY_NAME)
 
         for tmdb_id in tmdb_ids:
-            print(f'{qid},"""{tmdb_id}"""')
+            source = tmdb_id_sources[tmdb_id]
+            edit_summary = f"Add TMDb TV series ID claim via associated {source}"
+            print(
+                f"wd:{qid} "
+                f'wdt:{TMDB_TV_SERIES_ID_PID} "{tmdb_id}" ; '
+                f'wikidatabots:editSummary "{edit_summary}" .'
+            )
 
 
 if __name__ == "__main__":
