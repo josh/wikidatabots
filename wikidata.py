07916f731123768f79527e74d11d0a3c886affdd
Joshua Peek <josh@users.noreply.github.com>
2022-11-29 22:37:55 +0000

    Add more namespaces

--- wikidata.py
+++ wikidata.py
@@ -13,6 +13,7 @@ PR = Namespace("http://www.wikidata.org/prop/reference/")
 PS = Namespace("http://www.wikidata.org/prop/statement/")
 PSN = Namespace("http://www.wikidata.org/prop/statement/value-normalized/")
 PSV = Namespace("http://www.wikidata.org/prop/statement/value/")
+PROV = Namespace("http://www.w3.org/ns/prov#")
 RDF = Namespace("http://www.w3.org/1999/02/22-rdf-syntax-ns#")
 RDFS = Namespace("http://www.w3.org/2000/01/rdf-schema#")
 SCHEMA = Namespace("http://schema.org/")
@@ -27,6 +28,7 @@ WIKIBASE = Namespace("http://wikiba.se/ontology#")
 GENID = Namespace("http://www.wikidata.org/.well-known/genid/")
 
 WIKIDATABOTS = Namespace("https://github.com/josh/wikidatabots#")
+
 
 PID = NewType("PID", str)
 QID = NewType("QID", str)
@@ -56,6 +58,10 @@ def parse_uriref(uri: str) -> URIRef:
         return WDTURIRef(uri)
     elif uri.startswith(WD):
         return WDURIRef(uri)
+    elif uri.startswith(PROV):
+        return PROVURIRef(uri)
+    elif uri.startswith(PR):
+        return PRURIRef(uri)
     elif uri.startswith(PS):
         return PSURIRef(uri)
     elif uri.startswith(PQ):
@@ -88,7 +94,7 @@ class WikidataURIRef(URIRef):
     def __init__(self, value: str):
         assert value.startswith(self.namespace)
         URIRef.__init__(self)
-        assert "/" not in self.local_name()
+        assert "/" not in self.local_name(), self
 
     def local_name(self) -> str:
         start = len(self.namespace)
@@ -96,6 +102,11 @@ class WikidataURIRef(URIRef):
 
     def qname(self) -> str:
         return f"{self.prefix}:{self.local_name()}"
+
+
+class PROVURIRef(WikidataURIRef):
+    prefix: str = "prov"
+    namespace: Namespace = PROV
 
 
 class PURIRef(WikidataURIRef):
@@ -106,6 +117,11 @@ class PURIRef(WikidataURIRef):
 class PQURIRef(WikidataURIRef):
     prefix: str = "pq"
     namespace: Namespace = PQ
+
+
+class PRURIRef(WikidataURIRef):
+    prefix: str = "pr"
+    namespace: Namespace = PR
 
 
 class PSURIRef(WikidataURIRef):
