2a5ad882d105e9f53629fdd79b626de4a518c5b5
Joshua Peek <josh@users.noreply.github.com>
2022-12-08 18:50:58 +0000

    Keep all columns from export

---
+++ pyarrow_utils.py
@@ -0,0 +1,12 @@
+# pyright: basic
+
+import pyarrow.compute as pc
+import pyarrow as pa
+
+
+def fill_ids(table: pa.Table, key: str = "id") -> pa.Table:
+    index = table[key]
+    size = pc.max(index).as_py() + 1  # type: ignore
+    filled_index = pa.array(range(size), type=index.type)
+    filled_table = pa.table([filled_index], names=[key])
+    return table.join(filled_table, key, join_type="right outer").sort_by(key)
