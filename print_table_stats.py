03450df86fcedb9aa125b412aa19960a10424ad0
Joshua Peek <josh@users.noreply.github.com>
2022-12-08 01:25:50 +0000

    Print count once

--- print_table_stats.py
+++ print_table_stats.py
@@ -11,11 +11,11 @@ filename = sys.argv[1]
 start = time.time()
 table = feather.read_table(filename, memory_map=False)
 elapsed = time.time() - start
+count = len(table)
 
 for column_name in table.column_names:
     col = table[column_name]
     ary = col.combine_chunks()
-    count = len(ary)
 
     print(f"{column_name}: {col.type}")
     if col.type == pa.bool_():
@@ -23,14 +23,14 @@ for column_name in table.column_names:
         print(f"|  false: {ary.false_count:,} ({ary.false_count/count:.2%})")
 
     print(f"|   null: {ary.null_count:,} ({ary.null_count/count:.2%})")
-    print(f"|- total: {count:,}")
     print()
 
-print(f"load: {elapsed:0.2}s")
+print(f"total: {count:,}")
+print(f" load: {elapsed:0.2}s")
 
 kb = pa.total_allocated_bytes() >> 10
 mb = pa.total_allocated_bytes() >> 20
 if mb > 2:
-    print(f" rss: {mb:,}MB")
+    print(f"  rss: {mb:,}MB")
 else:
-    print(f" rss: {kb:,}KB")
+    print(f"  rss: {kb:,}KB")
