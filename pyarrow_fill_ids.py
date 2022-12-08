29e14c8fbd106c965e21a45090f5ffc5f648fcf8
Joshua Peek <josh@users.noreply.github.com>
2022-12-08 19:16:16 +0000

    Call cli tool

--- pyarrow_fill_ids.py
+++ pyarrow_fill_ids.py
@@ -2,9 +2,12 @@
 
 import sys
 
-from pyarrow_utils import read_json, write_feather
+from pyarrow_utils import fill_ids, read_table, write_feather
 
 if __name__ == "__main__":
     input_filename = sys.argv[1] if len(sys.argv) > 1 else "/dev/stdin"
     output_filename = sys.argv[2] if len(sys.argv) > 2 else "/dev/stdout"
-    write_feather(read_json(input_filename), output_filename)
+
+    table = read_table(input_filename)
+    table = fill_ids(table)
+    write_feather(table, output_filename)
