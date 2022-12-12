9451b35e5807d154069b3a4dfc36b5ff6ea1601a
Joshua Peek <josh@users.noreply.github.com>
2022-12-12 21:01:15 +0000

    Include id column in changed_at

--- tmdb_update_changed_at.py
+++ tmdb_update_changed_at.py
@@ -14,28 +14,39 @@ main_filename = sys.argv[1]
 changes_filename = sys.argv[2]
 
 table = feather.read_table(main_filename)
-changed_at = ma.masked_array(
+changed_at_col = ma.masked_array(
     table["changed_at"].to_numpy(),
     mask=table["changed_at"].is_null().to_numpy(),
+)
+adult_col = ma.masked_array(
+    table["adult"].to_numpy(),
+    mask=table["adult"].is_null().to_numpy(),
 )
 
 changes_table = feather.read_table(changes_filename)
 changed_ids = changes_table["id"].to_numpy()
+changed_adult = changes_table["adult"].to_numpy()
 datestr = os.path.basename(changes_filename).removesuffix(".arrow")
 date = np.datetime64(datestr, "D")
 
-changed_at = ma_reserve_capacity(changed_at, changed_ids.max() + 1)
+new_size = changed_ids.max() + 1
+changed_at_col = ma_reserve_capacity(changed_at_col, new_size)
+adult_col = ma_reserve_capacity(adult_col, new_size)
+id_col = pa.array(range(len(changed_at_col)), type=pa.int64())
 
 updated_rows = 0
 
-for id in changed_ids:
-    if changed_at[id] is ma.masked or changed_at[id] < date:
-        changed_at[id] = date
+for idx, id in enumerate(changed_ids):
+    if changed_at_col[id] is ma.masked or changed_at_col[id] < date:
+        changed_at_col[id] = date
+        adult_col[id] = changed_adult[idx]
         updated_rows += 1
 
 if updated_rows:
-    print(f"{datestr}: updated rows: {updated_rows}", file=sys.stderr)
-    table = pa.table([changed_at], names=["changed_at"])
+    table = pa.table(
+        [id_col, changed_at_col, adult_col],
+        names=["id", "changed_at", "adult"],
+    )
     feather.write_feather(table, main_filename)
 else:
     print(f"{datestr}: no rows updated", file=sys.stderr)
