063f8cbd035839153487ceeb065b875af90f0676
Joshua Peek <josh@users.noreply.github.com>
2022-12-13 00:57:26 +0000

    Process changes in batch

--- tmdb_append_changes.py
+++ tmdb_append_changes.py
@@ -3,25 +3,33 @@ import sys
 from datetime import date
 
 import pandas as pd
+from glob import glob
+
+removed = 0
+added = 0
 
 filename_existing = sys.argv[1]
-df_existing = pd.read_feather(filename_existing)
+df = pd.read_feather(filename_existing)
 
-filename_new = sys.argv[2]
-new_datestar = os.path.basename(filename_new).removesuffix(".arrow")
-new_date = date.fromisoformat(new_datestar)
-df_new = pd.read_feather(filename_new)
-df_new["date"] = new_date
+root_dir = sys.argv[2]
+filenames = sorted(glob("*.arrow", root_dir=root_dir))
+dates = [date.fromisoformat(filename.split(".")[0]) for filename in filenames]
 
-rows_to_drop = df_existing[df_existing["date"] == new_date].index
-if len(rows_to_drop):
-    df_existing = df_existing.drop(rows_to_drop)
-    print(f"-{len(rows_to_drop)} rows", file=sys.stderr)
+rows_to_drop = df[df["date"].isin(dates)].index
+removed = len(rows_to_drop)
+df = df.drop(rows_to_drop)
 
-df3 = pd.concat([df_existing, df_new])
-print(f"+{len(df_new)} rows", file=sys.stderr)
+dfs = [df]
+for idx, filename in enumerate(filenames):
+    df_new = pd.read_feather(os.path.join(root_dir, filename))
+    df_new["date"] = dates[idx]
+    dfs.append(df_new)
+    added += len(df_new)
 
-df3 = df3.sort_values(by=["date"], kind="stable")
-df3 = df3.reset_index(drop=True)
 
-df3.to_feather(filename_existing)
+df = pd.concat(dfs)
+df = df.sort_values(by=["date"], kind="stable")
+df = df.reset_index(drop=True)
+
+print(f"+{added:,}/-{removed:,} rows", file=sys.stderr)
+df.to_feather(filename_existing)
