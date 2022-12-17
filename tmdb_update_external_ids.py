4478b069298b77ea2b54f8258a0a173789ef1466
Joshua Peek <josh@users.noreply.github.com>
2022-12-17 21:36:56 +0000

    Use df to map to numeric id

--- tmdb_update_external_ids.py
+++ tmdb_update_external_ids.py
@@ -9,13 +9,9 @@ import pandas as pd
 import pyarrow as pa
 import pyarrow.feather as feather
 
-import imdb
 from utils import np_reserve_capacity
 
-imdb_type = sys.argv[1]
-assert imdb_type in ("tt", "nm")
-
-changed_json_dirname = sys.argv[3]
+changed_json_dirname = sys.argv[2]
 changed_json_filenames = sorted(glob("*.json", root_dir=changed_json_dirname))
 changed_json_ids = [int(fn.split(".")[0]) for fn in changed_json_filenames]
 
@@ -24,15 +20,20 @@ for idx, fn in enumerate(changed_json_filenames):
     dfx = pd.read_json(os.path.join(changed_json_dirname, fn), lines=True)
     dfx["id"] = changed_json_ids[idx]
     dfs.append(dfx)
+
 df = pd.concat(dfs, ignore_index=True)
 df = df.drop(columns=["success", "status_code", "status_message"], errors="ignore")
+df["imdb_numeric_id"] = (
+    df["imdb_id"].str.removeprefix("tt").str.removeprefix("nm").astype("Int64")
+)
+df["retrieved_at"] = np.datetime64("now")
 print(df, file=sys.stderr)
 
 imdb_ids = np.zeros(0, np.uint32)
 tvdb_ids = np.zeros(0, np.uint32)
 timestamps = np.empty(0, "datetime64[s]")
 
-filename = sys.argv[2]
+filename = sys.argv[1]
 table = feather.read_table(filename)
 imdb_ids = table.column("imdb_id").to_numpy().copy()
 if "tvdb_id" in table.column_names:
@@ -45,13 +46,13 @@ for row in df.itertuples():
     tvdb_ids = np_reserve_capacity(tvdb_ids, size, 0)
     timestamps = np_reserve_capacity(timestamps, size, np.datetime64("nat"))
 
-    if not pd.isna(row.imdb_id):
-        imdb_ids[row.id] = imdb.decode_numeric_id(row.imdb_id, imdb_type) or 0
+    if not pd.isna(row.imdb_numeric_id):
+        imdb_ids[row.id] = row.imdb_numeric_id
 
-    if "tvdb_id" in row:
-        if not pd.isna(row.tvdb_id):
-            tvdb_ids[row.id] = row.tvdb_id
-    timestamps[row.id] = np.datetime64("now")
+    if "tvdb_id" in df and not pd.isna(row.tvdb_id):
+        tvdb_ids[row.id] = row.tvdb_id
+
+    timestamps[row.id] = row.retrieved_at
 
 cols = []
 names = []
@@ -67,4 +68,4 @@ names.append("retrieved_at")
 cols.append(timestamps)
 
 table = pa.table(cols, names=names)
-feather.write_feather(table, filename)
+# feather.write_feather(table, filename)
