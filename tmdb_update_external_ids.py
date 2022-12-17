05254455bf17a2a7519c94b001caa5da5d3fd32a
Joshua Peek <josh@users.noreply.github.com>
2022-12-17 23:09:58 +0000

    Refactor

--- tmdb_update_external_ids.py
+++ tmdb_update_external_ids.py
@@ -20,27 +20,15 @@ for idx, fn in enumerate(changed_json_filenames):
     changed_dfs.append(dfx)
 
 changed_df = pd.concat(changed_dfs)
-
 changed_df["imdb_numeric_id"] = pd.to_numeric(
     changed_df["imdb_id"].str.removeprefix("tt").str.removeprefix("nm"),
     errors="coerce",
 ).astype("Int64")
-
 changed_df["retrieved_at"] = pd.Timestamp.now().floor("s")
-
-if "tvdb_id" in changed_df:
-    changed_df = changed_df[["imdb_id", "imdb_numeric_id", "tvdb_id", "retrieved_at"]]
-else:
-    changed_df = changed_df[["imdb_id", "imdb_numeric_id", "retrieved_at"]]
+changed_df = changed_df[df.columns]
 
 print(changed_df, file=sys.stderr)
 
-existing_rows = df.index.isin(changed_df.index)
-print(f"Dropping {len(df[existing_rows]):,} existing rows", file=sys.stderr)
-
-df = pd.concat([df[~existing_rows], changed_df])
+df = pd.concat([df[~df.index.isin(changed_df.index)], changed_df])
 df = df.sort_index().reset_index(names=["id"])
-
-print(f"Would write {len(df):,} rows", file=sys.stderr)
-print(df, file=sys.stderr)
 df.to_feather(sys.argv[1])
