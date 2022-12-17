ac02a938200835481aa1964cd44bd84b08c6447c
Joshua Peek <josh@users.noreply.github.com>
2022-12-17 22:55:20 +0000

    Dropping old imdb_id column

--- tmdb_update_external_ids.py
+++ tmdb_update_external_ids.py
@@ -21,7 +21,7 @@ for idx, fn in enumerate(changed_json_filenames):
 
 changed_df = pd.concat(changed_dfs)
 
-changed_df["imdb_id"] = pd.to_numeric(
+changed_df["imdb_numeric_id"] = pd.to_numeric(
     changed_df["imdb_id"].str.removeprefix("tt").str.removeprefix("nm"),
     errors="coerce",
 ).astype("Int64")
@@ -39,11 +39,10 @@ print(f"Dropping {len(df[existing_rows]):,} existing rows", file=sys.stderr)
 df = pd.concat([df[~existing_rows], changed_df])
 df = df.sort_index().reset_index(names=["id"])
 
-df["imdb_numeric_id"] = df["imdb_id"]
 if "tvdb_id" in df:
-    df = df[["id", "imdb_id", "imdb_numeric_id", "tvdb_id", "retrieved_at"]]
+    df = df[["id", "imdb_numeric_id", "tvdb_id", "retrieved_at"]]
 else:
-    df = df[["id", "imdb_id", "imdb_numeric_id", "retrieved_at"]]
+    df = df[["id", "imdb_numeric_id", "retrieved_at"]]
 
 print(f"Would write {len(df):,} rows", file=sys.stderr)
 print(df)
