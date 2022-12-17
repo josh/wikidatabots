82039f6a120495eeb2fd51493a26d6bb4ea8bdc9
Joshua Peek <josh@users.noreply.github.com>
2022-12-17 20:47:53 +0000

    Round retrieved at down to date

--- tmdb_external_ids_outdated.py
+++ tmdb_external_ids_outdated.py
@@ -10,5 +10,7 @@ df2 = (
 )
 
 df = pd.read_feather(sys.argv[1], columns=["retrieved_at"]).join(df2, how="left")
-df = df[(df["retrieved_at"].isna()) | (df["date"] >= df["retrieved_at"])]
+is_missing = df["retrieved_at"].isna()
+is_outdated = df["date"] >= df["retrieved_at"].dt.floor(freq="D")
+df = df[is_missing | is_outdated]
 df.to_csv(sys.stdout.buffer, columns=[], header=False)
