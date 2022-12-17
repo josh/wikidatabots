105196032e39a9eff3002ba525bdfa6d52faeafc
Joshua Peek <josh@users.noreply.github.com>
2022-12-17 23:19:21 +0000

    Force indexing new columns

--- tmdb_update_external_ids.py
+++ tmdb_update_external_ids.py
@@ -8,6 +8,26 @@ import pandas as pd
 
 df = pd.read_feather(sys.argv[1]).set_index("id")
 
+if "wikidata_id" not in df.columns:
+    df["wikidata_id"] = pd.Series(dtype="string")
+else:
+    df["wikidata_id"] = df["wikidata_id"].astype("string")
+
+if "facebook_id" not in df.columns:
+    df["facebook_id"] = pd.Series(dtype="string")
+else:
+    df["facebook_id"] = df["facebook_id"].astype("string")
+
+if "instagram_id" not in df.columns:
+    df["instagram_id"] = pd.Series(dtype="string")
+else:
+    df["instagram_id"] = df["instagram_id"].astype("string")
+
+if "twitter_id" not in df.columns:
+    df["twitter_id"] = pd.Series(dtype="string")
+else:
+    df["twitter_id"] = df["twitter_id"].astype("string")
+
 changed_json_dirname = sys.argv[2]
 changed_json_filenames = sorted(glob("*.json", root_dir=changed_json_dirname))
 changed_json_ids = [int(fn.split(".")[0]) for fn in changed_json_filenames]
@@ -31,4 +51,5 @@ print(changed_df, file=sys.stderr)
 
 df = pd.concat([df[~df.index.isin(changed_df.index)], changed_df])
 df = df.sort_index().reset_index(names=["id"])
+print(df)
 df.to_feather(sys.argv[1])
