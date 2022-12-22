4e786520e9df2234a21fd617259a140f4a99820e
Joshua Peek <josh@users.noreply.github.com>
2022-12-22 01:09:37 +0000

    Compact int types

--- tmdb_update_external_ids.py
+++ tmdb_update_external_ids.py
@@ -7,25 +7,32 @@ import pandas as pd
 from jsondir import read_json_dir_as_df
 
 df = input_df = pd.read_feather(sys.argv[1])
+
+# TMP migration
+df["id"] = df["id"].astype("UInt32")
+df["imdb_numeric_id"] = df["imdb_numeric_id"].astype("UInt32")
+if "tvdb_id" in df:
+    df["tvdb_id"] = df["tvdb_id"].astype("UInt32")
+
 df = df.set_index("id")
 
 changed_dtype = {
-    "id": "Int64",
+    "id": "UInt32",
     "imdb_id": "string",
-    "tvdb_id": "Int64",
+    "tvdb_id": "UInt32",
     "wikidata_id": "string",
     "facebook_id": "string",
     "instagram_id": "string",
     "twitter_id": "string",
     "success": "boolean",
-    "status_code": "Int64",
+    "status_code": "UInt8",
     "status_message": "string",
 }
 
 changed_df = (
     read_json_dir_as_df(sys.argv[2], dtype=changed_dtype)
     .rename(columns={"filename": "id", "id": "id_"})
-    .astype({"id": "int64"})
+    .astype({"id": "uint32"})
     .set_index("id")
 )
 
@@ -36,7 +43,7 @@ changed_df["success"] = changed_df["success"].fillna(True).astype("boolean")
 changed_df["imdb_numeric_id"] = pd.to_numeric(
     changed_df["imdb_id"].str.removeprefix("tt").str.removeprefix("nm"),
     errors="coerce",
-).astype("Int64")
+).astype("UInt32")
 changed_df["retrieved_at"] = pd.Timestamp.now().floor("s")
 changed_df = changed_df[df.columns]
 
@@ -45,11 +52,12 @@ print(changed_df, file=sys.stderr)
 df = pd.concat([df[~df.index.isin(changed_df.index)], changed_df])
 df = df.sort_index().reset_index(names=["id"])
 
-assert (
-    df.columns.tolist() == input_df.columns.tolist()
-), f"{df.columns} != {input_df.columns}"
-assert (
-    df.dtypes.tolist() == input_df.dtypes.tolist()
-), f"{df.dtypes} != {input_df.dtypes}"
+# TMP disable during migration
+# assert (
+#     df.columns.tolist() == input_df.columns.tolist()
+# ), f"{df.columns} != {input_df.columns}"
+# assert (
+#     df.dtypes.tolist() == input_df.dtypes.tolist()
+# ), f"{df.dtypes} != {input_df.dtypes}"
 
 df.to_feather(sys.argv[1])
