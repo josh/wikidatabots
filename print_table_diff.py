c666d1775ce6764489272c00b9c19eb9043a7bb3
Joshua Peek <josh@users.noreply.github.com>
2022-12-28 01:42:13 +0000

    Print df changes

---
+++ print_table_diff.py
@@ -0,0 +1,14 @@
+# pyright: strict
+
+import sys
+
+import pandas as pd
+
+from pandas_utils import df_diff
+
+key = sys.argv[1]
+df_a = pd.read_feather(sys.argv[2])
+df_b = pd.read_feather(sys.argv[3])
+
+(added, removed, updated) = df_diff(df_a, df_b, key=key)
+print(f"+{added:,} -{removed:,} ~{updated:,}")
