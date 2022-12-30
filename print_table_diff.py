cf4f6b3d46d3d02a22360ecfa557dbfb185b0395
Joshua Peek <josh@users.noreply.github.com>
2022-12-30 22:22:00 +0000

    Write diff to summary

--- print_table_diff.py
+++ print_table_diff.py
@@ -1,10 +1,16 @@
 # pyright: basic
 
+import os
 import sys
 
 import pandas as pd
 
 from pandas_utils import df_diff
+
+STEP_SUMMARY = os.environ.get("GITHUB_STEP_SUMMARY", "/dev/null")
+
+txt_out = sys.stdout
+md_out = open(STEP_SUMMARY, "w")
 
 df_a = pd.read_feather(sys.argv[1])
 df_b = pd.read_feather(sys.argv[2])
@@ -13,4 +19,7 @@ if len(sys.argv) > 3:
     key = sys.argv[3]
 
 (added, removed, updated) = df_diff(df_a, df_b, on=key)
-print(f"+{added:,} -{removed:,} ~{updated:,}")
+print(f"+{added:,} -{removed:,} ~{updated:,}", file=txt_out)
+
+print(f"## {sys.argv[1]} vs {sys.argv[2]}", file=md_out)
+print(f"+{added:,} -{removed:,} ~{updated:,}", file=md_out)
