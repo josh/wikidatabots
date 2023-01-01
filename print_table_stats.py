691e9ec4754c0557d2eecd10c685027264939af3
Joshua Peek <josh@users.noreply.github.com>
2023-01-01 01:02:33 +0000

    Add value counts to stats

--- print_table_stats.py
+++ print_table_stats.py
@@ -1,6 +1,5 @@
 # pyright: basic
 
-
 import os
 import sys
 
@@ -15,41 +14,53 @@ md_out = open(STEP_SUMMARY, "w")
 
 filename = sys.argv[1]
 
-table = feather.read_table(filename, memory_map=False)
-count = len(table)
-
+table = feather.read_table(filename)
 df = pd.read_feather(filename)
+count = len(df)
+
+schema_df = pd.DataFrame({"name": table.column_names}, index=table.column_names)
+schema_df["pyarrow"] = schema_df.name.apply(lambda n: table[n].type)
+schema_df["pandas"] = df.dtypes
+schema_df["true_count"] = df.select_dtypes(include="bool").where(df == 1).notna().sum()
+schema_df["false_count"] = df.select_dtypes(include="bool").where(df == 0).notna().sum()
+schema_df["null_count"] = df.isna().sum()
+schema_df["true_percent"] = schema_df.true_count / count
+schema_df["false_percent"] = schema_df.false_count / count
+schema_df["null_percent"] = schema_df.null_count / count
+
+for row in schema_df.itertuples():
+    print(f"{row.Index}: {row.pyarrow}[pyarrow] / {row.pandas}[pandas]", file=txt_out)
+    if row.true_count > 0 or row.false_count > 0:
+        print(
+            f"|   true: {row.true_count:,} ({row.true_percent:.2%})",
+            file=txt_out,
+        )
+        print(
+            f"|  false: {row.false_count:,} ({row.false_percent:.2%})",
+            file=txt_out,
+        )
+
+    if row.null_count > 0:
+        print(
+            f"|   null: {row.null_count:,} ({row.null_percent:.2%})",
+            file=txt_out,
+        )
 
 print(f"## {filename}", file=md_out)
-print("|name|pyarrow|pandas|", file=md_out)
-print("|---|---|---|", file=md_out)
-
-for column_name in table.column_names:
-    col = table[column_name]
-    ary = col.combine_chunks()
-
-    if df.index.name == column_name:
-        pd_dtype = df.index.dtype
+print("|name|pyarrow|pandas|null|true|false|", file=md_out)
+print("|---|---|---|---|---|---|", file=md_out)
+for row in schema_df.itertuples():
+    print(f"|{row.name}|{row.pyarrow}|{row.pandas}", file=md_out, end="|")
+    if row.null_count > 0:
+        print(f"{row.null_count:,} ({row.null_percent:.2%})", file=md_out, end="|")
     else:
-        pd_dtype = df[column_name].dtype
-
-    print(f"{column_name}: {col.type}[pyarrow] / {pd_dtype}[pandas]", file=txt_out)
-    print(f"|{column_name}|`{col.type}`|`{pd_dtype}`|", file=md_out)
-    if col.type == pa.bool_():
-        print(
-            f"|   true: {ary.true_count:,} ({ary.true_count/count:.2%})",
-            file=txt_out,
-        )
-        print(
-            f"|  false: {ary.false_count:,} ({ary.false_count/count:.2%})",
-            file=txt_out,
-        )
-
-    if ary.null_count:
-        print(
-            f"|   null: {ary.null_count:,} ({ary.null_count/count:.2%})",
-            file=txt_out,
-        )
+        print("", file=md_out, end="|")
+    if row.true_count > 0 or row.false_count > 0:
+        print(f"{row.true_count:,} ({row.true_percent:.2%})", file=md_out, end="|")
+        print(f"{row.false_count:,} ({row.false_percent:.2%})", file=md_out, end="|")
+    else:
+        print("||", file=md_out, end="")
+    print("", file=md_out)
 
 print(f"total: {count:,}", file=txt_out)
 
