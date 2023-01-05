c7d4a08be28435832995fabfbba91dd93711c26a
Joshua Peek <josh@users.noreply.github.com>
2023-01-05 01:30:01 +0000

    Fetch guids

--- plex_etl/__init__.py
+++ plex_etl/__init__.py
@@ -1,5 +1,6 @@
 # pyright: strict
 
+from .plex_library import plex_library_guids
 from .wikidata import wd_plex_guids
 
-__all__ = ["wd_plex_guids"]
+__all__ = ["plex_library_guids", "wd_plex_guids"]
---
+++ plex_etl/plex_library.py
@@ -0,0 +1,27 @@
+# pyright: basic
+
+import os
+
+import pandas as pd
+from plexapi.myplex import MyPlexAccount
+
+from .utils import decode_plex_guids
+
+
+def plex_connect():
+    account = MyPlexAccount(
+        username=os.environ["PLEX_USERNAME"],
+        password=os.environ["PLEX_PASSWORD"],
+        token=os.environ["PLEX_TOKEN"],
+    )
+    resource = account.resource(os.environ["PLEX_SERVER"])
+    return resource.connect()
+
+
+def plex_library_guids():
+    plex = plex_connect()
+    guids = pd.Series([item.guid for item in plex.library.all()])
+    df = decode_plex_guids(guids)
+    df["guid"] = pd.Series(guids, dtype="string")
+    df = df.dropna()
+    return df[["guid", "type", "key"]]
