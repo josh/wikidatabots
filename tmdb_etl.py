28281c175ebd29499efcca327d60f06acd0611c6
Joshua Peek <josh@users.noreply.github.com>
2023-01-18 04:55:53 +0000

    Rework external id fetcher

--- tmdb_etl.py
+++ tmdb_etl.py
@@ -7,7 +7,7 @@ import pandas as pd
 import requests
 from tqdm import tqdm
 
-from pandas_utils import compact_dtypes, safe_row_concat
+from pandas_utils import compact_dtypes, read_json_series, safe_row_concat
 
 
 def check_tmdb_changes_schema(df: pd.DataFrame) -> None:
@@ -18,6 +18,26 @@ def check_tmdb_changes_schema(df: pd.DataFrame) -> None:
     assert df.dtypes["adult"] == "boolean", f"adult dtype is {df.dtypes['adult']}"
     assert len(df) > 0, "empty dataframe"
     assert df["date"].is_monotonic_increasing, "dates are not sorted"
+
+
+def check_tmdb_external_ids_schema(df: pd.DataFrame) -> None:
+    assert df.dtypes["id"] == "uint32", f"id dtype is {df.dtypes['id']}"
+    assert df.dtypes["imdb_id"] == "string", f"imdb_id dtype is {df.dtypes['imdb_id']}"
+
+    assert (
+        df.dtypes["imdb_numeric_id"] == "UInt32"
+    ), f"imdb_numeric_id dtype is {df.dtypes['imdb_numeric_id']}"
+
+    assert (
+        df.dtypes["wikidata_id"] == "string"
+    ), f"wikidata_id dtype is {df.dtypes['wikidata_id']}"
+
+    if "tvdb_id" in df.columns:
+        assert (
+            df.dtypes["tvdb_id"] == "UInt32"
+        ), f"tvdb_id dtype is {df.dtypes['tvdb_id']}"
+
+    assert len(df) > 0, "empty dataframe"
 
 
 def tmdb_changes(date: date, tmdb_type: str) -> pd.DataFrame:
@@ -66,4 +86,96 @@ def insert_tmdb_changes(df: pd.DataFrame, tmdb_type: str):
 
     check_tmdb_changes_schema(df)
     assert len(df) >= initial_size, f"before {initial_size} rows, after {len(df)} rows"
+    return df
+
+
+def tmdb_outdated_external_ids(
+    changes_df: pd.DataFrame,
+    external_ids_df: pd.DataFrame,
+) -> pd.Series:
+    latest_changes_df = (
+        changes_df.drop_duplicates(subset=["id"], keep="last")
+        .set_index("id")
+        .sort_index()
+    )
+
+    external_ids_df = external_ids_df.set_index("id")
+
+    df = external_ids_df.join(latest_changes_df, how="left")
+    is_missing = df["retrieved_at"].isna()
+    is_outdated = df["date"] >= df["retrieved_at"].dt.floor(freq="D")
+    df = df[is_missing | is_outdated].reset_index()
+    return df["id"]
+
+
+def tmdb_external_ids_need_backfill(external_ids_df: pd.DataFrame) -> pd.Series:
+    df = external_ids_df.set_index("id")
+    df = df[df["success"].isna()].reset_index()
+    return df["id"].head(5000)
+
+
+EXTERNAL_IDS_DTYPES = {
+    "id": "UInt32",
+    "imdb_id": "string",
+    "tvdb_id": "UInt32",
+    "wikidata_id": "string",
+    "facebook_id": "string",
+    "instagram_id": "string",
+    "twitter_id": "string",
+    "success": "boolean",
+    "status_code": "UInt8",
+    "status_message": "string",
+}
+
+
+def fetch_tmdb_external_ids(tmdb_ids: pd.Series, tmdb_type: str) -> pd.DataFrame:
+    assert isinstance(tmdb_ids.index, pd.RangeIndex), f"index is {type(tmdb_ids.index)}"
+
+    api_key = os.environ["TMDB_API_KEY"]
+    urls = (
+        "https://api.themoviedb.org/3/"
+        + tmdb_type
+        + "/"
+        + tmdb_ids.astype("string")
+        + "/external_ids"
+    )
+    params = {"api_key": api_key}
+
+    def fetch(url: str):
+        r = requests.get(url, params=params)
+        return r.text
+
+    tqdm.pandas(desc="Fetch TMDB external IDs")
+    jsonl = urls.progress_apply(fetch)  # type: ignore
+
+    df = read_json_series(jsonl, dtype=EXTERNAL_IDS_DTYPES)
+
+    df["id"] = tmdb_ids
+    df = df.set_index("id")
+
+    if "success" not in df:
+        df["success"] = True
+    df["success"] = df["success"].fillna(True).astype("boolean")
+
+    df["imdb_numeric_id"] = pd.to_numeric(
+        df["imdb_id"].str.removeprefix("tt").str.removeprefix("nm"),
+        errors="coerce",
+    ).astype("UInt32")
+
+    df["retrieved_at"] = pd.Timestamp.now().floor("s")
+
+    df = df.replace(to_replace="", value=pd.NA)
+
+    return df
+
+
+def insert_tmdb_external_ids(
+    df: pd.DataFrame, tmdb_type: str, tmdb_ids: pd.Series
+) -> pd.DataFrame:
+    df_orig = df.copy()
+    df_updated_rows = fetch_tmdb_external_ids(tmdb_type=tmdb_type, tmdb_ids=tmdb_ids)
+    assert df_updated_rows.index.isin(df.index).all()
+    df.loc[df_updated_rows.index] = df_updated_rows[df.columns]  # type: ignore
+    assert len(df) == len(df_orig)
+    assert df.columns.equals(df_orig.columns)
     return df
--- tmdb_external_ids_backfill.py
+++
@@ -1,7 +0,0 @@
-import sys
-
-import pandas as pd
-
-df = pd.read_feather(sys.argv[1])
-df = df[df["success"].isna()]
-df.head(5000).to_csv(sys.stdout.buffer, columns=[], header=False)
