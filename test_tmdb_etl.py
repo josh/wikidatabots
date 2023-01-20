8c364116d27082c64c462dc3d908557a92d8dc46
Joshua Peek <josh@users.noreply.github.com>
2023-01-20 02:31:13 +0000

    Refactor into polars

--- test_tmdb_etl.py
+++ test_tmdb_etl.py
@@ -1,8 +1,73 @@
 import datetime
 
-from tmdb_etl import tmdb_changes
+import polars as pl
+from polars.testing import assert_frame_equal
+
+from tmdb_etl import (
+    fetch_tmdb_external_ids,
+    insert_tmdb_changes,
+    tmdb_changes,
+    tmdb_latest_changes,
+)
 
 
 def test_tmdb_changes():
     date = datetime.date(2023, 1, 1)
     tmdb_changes(date=date, tmdb_type="movie")
+
+
+def test_insert_tmdb_changes():
+    df1 = pl.DataFrame(
+        {
+            "date": [datetime.date.today()],
+            "id": [3],
+            "adult": [False],
+        },
+        columns={"date": pl.Date, "id": pl.UInt32, "adult": pl.Boolean},
+    )
+    df2 = insert_tmdb_changes(df1, tmdb_type="movie", days=0)
+    assert len(df2) > 0
+
+
+def test_tmdb_latest_changes():
+    df1 = pl.DataFrame(
+        {
+            "date": [
+                datetime.date.today(),
+                datetime.date.today(),
+                datetime.date.today(),
+            ],
+            "id": [1, 2, 1],
+            "adult": [False, False, True],
+        },
+        columns={"date": pl.Date, "id": pl.UInt32, "adult": pl.Boolean},
+    )
+    df2 = pl.DataFrame(
+        {
+            "id": [0, 1, 2],
+            "has_changes": [False, True, True],
+            "date": [None, datetime.date.today(), datetime.date.today()],
+            "adult": [None, True, False],
+        },
+        columns={
+            "id": pl.UInt32,
+            "has_changes": pl.Boolean,
+            "date": pl.Date,
+            "adult": pl.Boolean,
+        },
+    )
+    df3 = tmdb_latest_changes(df1)
+    assert_frame_equal(df2, df3)
+
+
+def test_fetch_tmdb_external_ids():
+    ids = pl.Series([1, 2, 3, 4])
+    df = fetch_tmdb_external_ids(ids, "movie").select(["id", "success", "imdb_id"])
+    df2 = pl.DataFrame(
+        {
+            "id": pl.Series([1, 2, 3, 4], dtype=pl.UInt32),
+            "success": [False, True, True, False],
+            "imdb_id": [None, "tt0094675", "tt0092149", None],
+        }
+    )
+    assert_frame_equal(df, df2)
--- tmdb_etl.py
+++ tmdb_etl.py
@@ -2,22 +2,11 @@ import datetime
 import os
 from datetime import timedelta
 
-import pandas as pd
 import polars as pl
 import requests
 from tqdm import tqdm
 
-import actions
-from pandas_utils import (
-    df_assign_or_append,
-    ensure_astype,
-    ensure_astypes,
-    read_json_series,
-    reindex_as_range,
-)
-from polars_utils import reindex_as_range as reindex_as_range_pl
-
-actions.install_warnings_hook()
+from polars_utils import reindex_as_range
 
 session = requests.Session()
 
@@ -43,11 +32,15 @@ def tmdb_changes(date: datetime.date, tmdb_type: str) -> pl.DataFrame:
     )
 
 
-def insert_tmdb_changes(df: pl.DataFrame, tmdb_type: str) -> pl.DataFrame:
+def insert_tmdb_changes(
+    df: pl.DataFrame,
+    tmdb_type: str,
+    days: int = 7,
+) -> pl.DataFrame:
     start_date = df["date"].max()
     assert isinstance(start_date, datetime.date)
     dates = pl.date_range(
-        low=start_date - datetime.timedelta(days=14),  # days=3
+        low=start_date - datetime.timedelta(days=days),
         high=datetime.date.today(),
         interval=datetime.timedelta(days=1),
         name="date",
@@ -63,136 +56,93 @@ def insert_tmdb_changes(df: pl.DataFrame, tmdb_type: str) -> pl.DataFrame:
 def tmdb_latest_changes(df: pl.DataFrame) -> pl.DataFrame:
     return (
         df.unique(subset=["id"], keep="last")
-        .pipe(reindex_as_range_pl, name="id")
+        .pipe(reindex_as_range, name="id")
         .with_column(pl.col("date").is_not_null().alias("has_changes"))
         .select(["id", "has_changes", "date", "adult"])
     )
 
 
 def tmdb_outdated_external_ids(
-    latest_changes_df: pd.DataFrame,
-    external_ids_df: pd.DataFrame,
-) -> pd.Series:
-    assert latest_changes_df.index.name == "id", "set index to id"
-    assert external_ids_df.index.name == "id", "set index to id"
-
-    df = latest_changes_df.join(external_ids_df, how="left")
-    is_missing = df["retrieved_at"].isna()
-    is_outdated = df["date"] >= df["retrieved_at"].dt.floor(freq="D")
-    df = df[is_missing | is_outdated]
-    return ensure_astype(df.index.to_series(), dtype="uint32")
+    latest_changes_df: pl.DataFrame,
+    external_ids_df: pl.DataFrame,
+) -> pl.Series:
+    df = latest_changes_df.join(external_ids_df, on="id", how="left")
+    is_missing = pl.col("retrieved_at").is_null()
+    is_outdated = pl.col("date") >= pl.col("retrieved_at").dt.round("1d")
+    return df.filter(is_missing | is_outdated)["id"].head(1_000)
 
 
-def tmdb_external_ids_need_backfill(external_ids_df: pd.DataFrame) -> pd.Series:
+def tmdb_external_ids_need_backfill(external_ids_df: pl.DataFrame) -> pl.Series:
     df = external_ids_df
-    assert df.index.name == "id", "set index to id"
-    return ensure_astype(df[df["success"].isna()].index.to_series(), dtype="uint32")
+    return df.filter(df["success"].is_null())["id"].head(1_000)
 
 
-EXTERNAL_IDS_DTYPES = {
-    "id": "UInt32",
-    "imdb_id": "string",
-    "tvdb_id": "UInt32",
-    "wikidata_id": "string",
-    "facebook_id": "string",
-    "instagram_id": "string",
-    "twitter_id": "string",
-    "success": "boolean",
-    "status_code": "UInt8",
-    "status_message": "string",
-}
+ExtractIMDbNumericIDExpr = (
+    pl.col("imdb_id")
+    .str.extract(r"(tt|nm)(\d+)", 2)
+    .cast(pl.UInt32)
+    .alias("imdb_numeric_id")
+)
 
 
-def fetch_tmdb_external_ids(tmdb_ids: pd.Series, tmdb_type: str) -> pd.DataFrame:
-    assert len(tmdb_ids) > 0, "no ids"
-    assert tmdb_ids.index.name == "id"
-    assert tmdb_ids.index.dtype == "uint64"
-    # assert tmdb_ids.equals(tmdb_ids.index.to_series())
-
+def fetch_tmdb_external_ids(tmdb_ids: pl.Series, tmdb_type: str) -> pl.DataFrame:
     api_key = os.environ["TMDB_API_KEY"]
-    urls = (
-        "https://api.themoviedb.org/3/"
-        + tmdb_type
-        + "/"
-        + tmdb_ids.astype("string")
-        + "/external_ids"
+
+    records = []
+    pbar = tqdm(tmdb_ids, desc="Fetch TMDB external IDs")
+    for tmdb_id in pbar:
+        url = f"https://api.themoviedb.org/3/{tmdb_type}/{tmdb_id}/external_ids"
+        r = session.get(url, params={"api_key": api_key})
+        data = r.json()
+        records.append(
+            {
+                "id": tmdb_id,
+                "success": data.get("success", True),
+                "retrieved_at": datetime.datetime.now(),
+                "imdb_id": data.get("imdb_id"),
+                "tvdb_id": data.get("tvdb_id"),
+                "wikidata_id": data.get("wikidata_id"),
+            }
+        )
+
+    schema = {
+        "id": pl.UInt32(),
+        "success": pl.Boolean(),
+        "retrieved_at": pl.Datetime(time_unit="ns"),
+        "imdb_id": pl.Utf8(),
+        "tvdb_id": pl.UInt32(),
+        "wikidata_id": pl.Utf8(),
+    }
+    return pl.from_dicts(records, schema=schema).with_columns(
+        [
+            pl.col("retrieved_at").dt.round("1s"),
+            ExtractIMDbNumericIDExpr,
+        ]
     )
-    params = {"api_key": api_key}
-
-    def fetch(url: str):
-        r = session.get(url, params=params)
-        return r.text
-
-    tqdm.pandas(desc="Fetch TMDB external IDs")
-    jsonl = urls.progress_apply(fetch)  # type: ignore
-
-    df = read_json_series(jsonl, dtype=EXTERNAL_IDS_DTYPES)
-
-    if "success" not in df:
-        df["success"] = True
-    df["success"] = df["success"].fillna(True).astype("boolean")
-
-    if "imdb_id" in df:
-        df["imdb_numeric_id"] = pd.to_numeric(
-            df["imdb_id"].str.removeprefix("tt").str.removeprefix("nm"),
-            errors="coerce",
-        ).astype("UInt32")
-
-    df["retrieved_at"] = pd.Timestamp.now().floor("s")
-
-    df = df.replace(to_replace="", value=pd.NA)
-
-    assert df.index.name == "id"
-    assert df.index.dtype == "uint64"
-    assert df.index.equals(tmdb_ids.index)
-    return df
 
 
 def insert_tmdb_external_ids(
-    df: pd.DataFrame,
+    df: pl.DataFrame,
     tmdb_type: str,
-    tmdb_ids: pd.Series,
-) -> pd.DataFrame:
-    if len(tmdb_ids) == 0:
-        return df
-    assert tmdb_ids.index.name == "id"
-    assert tmdb_ids.index.dtype == "uint64"
-    df_orig = df.copy()
+    tmdb_ids: pl.Series,
+) -> pl.DataFrame:
+    if "tvdb_id" not in df.columns:
+        df = df.with_column(pl.lit(None, dtype=pl.UInt32).alias("tvdb_id"))
+    df = df.select(
+        [
+            "id",
+            "success",
+            "retrieved_at",
+            "imdb_id",
+            "tvdb_id",
+            "wikidata_id",
+            "imdb_numeric_id",
+        ]
+    )
+
     df_updated_rows = fetch_tmdb_external_ids(tmdb_type=tmdb_type, tmdb_ids=tmdb_ids)
-    shared_columns = df_orig.columns.intersection(df_updated_rows.columns)
-    df = df_assign_or_append(df, df_updated_rows, shared_columns)
-    df = reindex_as_range(df)
-    assert len(df) >= len(df_orig)
-    assert df.columns.equals(df_orig.columns)
-    return df
-
-
-def set_id_index(df: pd.DataFrame) -> pd.DataFrame:
-    assert "id" in df.columns, "missing id column"
-    df = ensure_astypes(df, dtypes={"id": "uint32"})
-    assert isinstance(df.index, pd.RangeIndex), f"current index is {type(df.index)}"
-
-    df = df.set_index("id")
-
-    assert df.index.name == "id"
-    assert df.index.dtype == "uint64"
-
-    return df
-
-
-def unset_id_index(df: pd.DataFrame) -> pd.DataFrame:
-    assert "id" not in df.columns, "id column already exists"
-    assert df.index.name == "id", f"id name {df.index.name}"
-    # assert (
-    #     df.index.dtype == "uint32" or df.index.dtype == "uint64"
-    # ), f"id index {df.index.dtype}"
-
-    df = df.reset_index()
-    id_col = ensure_astype(df["id"], dtype="uint32")
-    df = df.drop(columns=["id"])
-    df.insert(0, "id", id_col)
-
-    assert isinstance(df.index, pd.RangeIndex)
-    assert df["id"].dtype == "uint32"
-
-    return df
+    return (
+        df.extend(df_updated_rows)
+        .unique(subset=["id"], keep="last")
+        .pipe(reindex_as_range, name="id")
+    )
