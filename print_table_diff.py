03a2393878299271719c2f79e6f358cb111a7eec
Joshua Peek <josh@users.noreply.github.com>
2023-01-20 01:21:10 +0000

    Ensure same string cache is used

--- print_table_diff.py
+++ print_table_diff.py
@@ -1,5 +1,3 @@
-# pyright: strict
-
 import os
 import sys
 
@@ -12,8 +10,9 @@ STEP_SUMMARY = os.environ.get("GITHUB_STEP_SUMMARY", "/dev/null")
 txt_out = sys.stdout
 md_out = open(STEP_SUMMARY, "w")
 
-df_a = pl.read_ipc(sys.argv[1], memory_map=False)
-df_b = pl.read_ipc(sys.argv[2], memory_map=False)
+with pl.StringCache():
+    df_a = pl.read_ipc(sys.argv[1], memory_map=False)
+    df_b = pl.read_ipc(sys.argv[2], memory_map=False)
 
 if len(sys.argv) > 3:
     key = sys.argv[3]
