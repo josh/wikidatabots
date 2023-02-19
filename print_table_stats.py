79257cf22208c1383d29a9d9ea80eadcad0c9a5e
Joshua Peek <josh@users.noreply.github.com>
2023-02-19 18:41:52 +0000

    Remove pandas dtype

--- print_table_stats.py
+++ print_table_stats.py
@@ -18,8 +18,7 @@ df = pl.read_ipc(filename, memory_map=False).to_pandas()
 count = len(df)
 
 schema_df = pd.DataFrame({"name": table.column_names}, index=table.column_names)
-schema_df["pyarrow"] = schema_df.name.apply(lambda n: table[n].type)
-schema_df["pandas"] = df.dtypes
+schema_df["dtype"] = schema_df.name.apply(lambda n: table[n].type)
 
 bool_colnames = [n for n in table.column_names if table[n].type == pa.bool_()]
 bool_cols = df[bool_colnames]
@@ -37,7 +36,7 @@ schema_df["null_percent"] = schema_df.null_count / count
 schema_df["is_unique"] = df.nunique(dropna=True) == (count - schema_df.null_count)
 
 for row in schema_df.itertuples():
-    print(f"{row.Index}: {row.pyarrow}[pyarrow] / {row.pandas}[pandas]", file=txt_out)
+    print(f"{row.Index}: {row.dtype}", file=txt_out)
     if row.true_count > 0 or row.false_count > 0:
         print(
             f"|   true: {row.true_count:,} ({row.true_percent:.2%})",
@@ -58,10 +57,10 @@ for row in schema_df.itertuples():
         print("|   unique", file=txt_out)
 
 print(f"## {filename}", file=md_out)
-print("|name|pyarrow|pandas|null|true|false|unique|", file=md_out)
+print("|name|dtype|null|true|false|unique|", file=md_out)
 print("|---|---|---|---|---|---|---|", file=md_out)
 for row in schema_df.itertuples():
-    print(f"|{row.name}|{row.pyarrow}|{row.pandas}", file=md_out, end="|")
+    print(f"|{row.name}|{row.dtype}", file=md_out, end="|")
     if row.null_count > 0:
         print(f"{row.null_count:,} ({row.null_percent:.2%})", file=md_out, end="|")
     else:
