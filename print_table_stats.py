b994cf849ba5b062e3c78d71995e0f5243456020
Joshua Peek <josh@users.noreply.github.com>
2023-03-23 21:53:10 +0000

    Use built in table formater

--- print_table_stats.py
+++ print_table_stats.py
@@ -1,14 +1,8 @@
 # pyright: strict
 
-import os
 import sys
 
 import polars as pl
-
-STEP_SUMMARY = os.environ.get("GITHUB_STEP_SUMMARY", "/dev/null")
-
-txt_out = sys.stdout
-md_out = open(STEP_SUMMARY, "w")
 
 filename = sys.argv[1]
 
@@ -34,7 +28,16 @@ false_count_df = count_columns(
     "false_count", pl.col(pl.Boolean).drop_nulls().is_not().sum()
 )
 
-schema_df = (
+
+def _comma(value: int) -> str:
+    return f"{value:,}"
+
+
+def _percent(value: float) -> str:
+    return f"{value:.2%}"
+
+
+summary_df = (
     (
         null_count_df.join(is_unique_df, on="column", how="left")
         .join(true_count_df, on="column", how="left")
@@ -45,70 +48,57 @@ schema_df = (
         pl.col("true_count").fill_null(0),
         pl.col("false_count").fill_null(0),
     )
-    .with_columns(
-        (pl.col("true_count") / count).alias("true_percent"),
-        (pl.col("false_count") / count).alias("false_percent"),
-        (pl.col("null_count") / count).alias("null_percent"),
+    .select(
+        pl.col("column").alias("name"),
+        pl.col("dtype").alias("dtype"),
+        pl.when(pl.col("null_count") > 0)
+        .then(
+            pl.format(
+                "{} ({})",
+                pl.col("null_count").apply(_comma, return_dtype=pl.Utf8),
+                (pl.col("null_count") / count).apply(_percent, return_dtype=pl.Utf8),
+            )
+        )
+        .otherwise("")
+        .alias("null"),
+        pl.when(pl.col("true_count") > 0)
+        .then(
+            pl.format(
+                "{} ({})",
+                pl.col("true_count").apply(_comma, return_dtype=pl.Utf8),
+                (pl.col("true_count") / count).apply(_percent, return_dtype=pl.Utf8),
+            )
+        )
+        .otherwise("")
+        .alias("true"),
+        pl.when(pl.col("false_count") > 0)
+        .then(
+            pl.format(
+                "{} ({})",
+                pl.col("false_count").apply(_comma, return_dtype=pl.Utf8),
+                (pl.col("false_count") / count).apply(_percent, return_dtype=pl.Utf8),
+            )
+        )
+        .otherwise("")
+        .alias("false"),
+        pl.when(pl.col("is_unique")).then("true").otherwise("").alias("unique"),
     )
 )
 
-for row in schema_df.iter_rows(named=True):
-    print(f"{row['column']}: {row['dtype']}", file=txt_out)
-    if row["true_count"] > 0 or row["false_count"] > 0:
-        print(
-            f"|   true: {row['true_count']:,} ({row['true_percent']:.2%})",
-            file=txt_out,
-        )
-        print(
-            f"|   false: {row['false_count']:,} ({row['false_percent']:.2%})",
-            file=txt_out,
-        )
 
-    if row["null_count"] > 0:
-        print(
-            f"|   null: {row['null_count']:,} ({row['null_percent']:.2%})",
-            file=txt_out,
-        )
+pl.Config.set_tbl_column_data_type_inline(True)
+pl.Config.set_tbl_formatting("ASCII_MARKDOWN")
+pl.Config.set_tbl_hide_dataframe_shape(True)
+pl.Config.set_tbl_rows(20)
 
-    if row["is_unique"]:
-        print("|   unique", file=txt_out)
 
-print(f"## {filename}", file=md_out)
-print("|name|dtype|null|true|false|unique|", file=md_out)
-print("|---|---|---|---|---|---|", file=md_out)
-for row in schema_df.iter_rows(named=True):
-    print(f"|{row['column']}|{row['dtype']}", file=md_out, end="|")
-    if row["null_count"] > 0:
-        print(
-            f"{row['null_count']:,} ({row['null_percent']:.2%})", file=md_out, end="|"
-        )
-    else:
-        print("", file=md_out, end="|")
-    if row["true_count"] > 0 or row["false_count"] > 0:
-        print(
-            f"{row['true_count']:,} ({row['true_percent']:.2%})", file=md_out, end="|"
-        )
-        print(
-            f"{row['false_count']:,} ({row['false_percent']:.2%})", file=md_out, end="|"
-        )
-    else:
-        print("||", file=md_out, end="")
-    if row["is_unique"]:
-        print("true", file=md_out, end="|")
-    else:
-        print("", file=md_out, end="|")
-    print("", file=md_out)
-
-print(f"total: {count:,}", file=txt_out)
-
-print("", file=md_out)
-print(f"total: {count:,} rows", file=md_out)
+print(f"## {filename}")
+print(summary_df)
+print(f"\nshape: {df.shape}")
 
 mb = df.estimated_size("mb")
 if mb > 2:
-    print(f"  rss: {mb:,.1f}MB", file=txt_out)
-    print(f"rss: {mb:,.1f}MB", file=md_out)
+    print(f"rss: {mb:,.1f}MB")
 else:
     kb = df.estimated_size("kb")
-    print(f"  rss: {kb:,.1f}KB", file=txt_out)
-    print(f"rss: {kb:,.1f}KB", file=md_out)
+    print(f"rss: {kb:,.1f}KB")
