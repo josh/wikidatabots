9cb73a2a4afdab6af7f07c8974452b847c3f2b0b
Joshua Peek <josh@users.noreply.github.com>
2023-04-08 02:26:58 +0000

    Expect new key

--- test_wikidata_etl.py
+++ test_wikidata_etl.py
@@ -6,6 +6,7 @@ from wikidata_etl import fetch_property_class_constraints
 def test_fetch_property_class_constraints() -> None:
     ldf = fetch_property_class_constraints("P4947")
     assert ldf.schema == {
+        "key": pl.Utf8,
         "numeric_pid": pl.UInt32,
         "pid": pl.Categorical,
         "class_numeric_qid": pl.UInt32,
