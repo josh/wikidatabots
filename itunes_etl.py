447fe63f6e7c47699c72d1af58d074e215e44386
Joshua Peek <josh@users.noreply.github.com>
2023-04-13 17:41:31 +0000

    WIP itunes lookup

---
+++ itunes_etl.py
@@ -0,0 +1,119 @@
+# pyright: strict
+
+from functools import partial
+
+import polars as pl
+
+from polars_requests import Session, prepare_request, response_text, urllib3_requests
+from polars_utils import expr_indicies_sorted, groups_of
+
+_LOOKUP_BATCH_SIZE = 150
+
+
+def lookup_itunes_id(expr: pl.Expr, batch_size: int = _LOOKUP_BATCH_SIZE) -> pl.Expr:
+    return expr.map(
+        partial(_lookup_itunes_id, batch_size=batch_size),
+        return_dtype=_LOOKUP_DTYPE,
+    )
+
+
+_RESULT_DTYPE = pl.Struct(
+    {
+        "wrapperType": pl.Utf8,
+        "artistType": pl.Utf8,
+        "artistId": pl.UInt64,
+        "artistName": pl.Utf8,
+        "collectionType": pl.Utf8,
+        "collectionId": pl.UInt64,
+        "collectionName": pl.Utf8,
+        "trackId": pl.UInt64,
+        "trackName": pl.Utf8,
+        "kind": pl.Utf8,
+    }
+)
+
+_RESULTS_DTYPE = pl.Struct({"results": pl.List(_RESULT_DTYPE)})
+
+_LOOKUP_DTYPE = pl.Struct(
+    [
+        pl.Field("id", pl.UInt64),  # ???
+        pl.Field("type", pl.Utf8),
+        pl.Field("name", pl.Utf8),
+        pl.Field("kind", pl.Utf8),
+    ]
+)
+
+_SESSION = Session(
+    connect_timeout=0.5,
+    read_timeout=1.0,
+    retry_count=2,
+)
+
+
+def _lookup_itunes_id(s: pl.Series, batch_size: int) -> pl.Series:
+    return (
+        s.to_frame("id")
+        .lazy()
+        .select(
+            pl.lit("https://itunes.apple.com/lookup")
+            .pipe(
+                prepare_request,
+                fields={
+                    "id": pl.col("id")
+                    .pipe(groups_of, n=batch_size)
+                    .cast(pl.List(pl.Utf8))
+                    .arr.join(separator=","),
+                    "country": "us",
+                },
+            )
+            .pipe(urllib3_requests, session=_SESSION, log_group="itunes_lookup")
+            .pipe(response_text)
+            .str.json_extract(_RESULTS_DTYPE)
+            .struct.field("results")
+            .arr.eval(pl.element().pipe(_lookup_result))
+            .flatten()
+            .sort()
+            .alias("result")
+        )
+        .select(
+            pl.col("result").take(
+                expr_indicies_sorted(
+                    pl.lit(s),
+                    pl.col("result").struct.field("id"),
+                )
+            )
+        )
+        .collect()
+        .to_series()
+    )
+
+
+def _lookup_result(expr: pl.Expr) -> pl.Expr:
+    return (
+        pl.when(expr.struct.field("wrapperType") == "collection")
+        .then(
+            pl.struct(
+                expr.struct.field("collectionId").alias("id"),
+                expr.struct.field("collectionType").alias("type"),
+                expr.struct.field("collectionName").alias("name"),
+                pl.lit(None).alias("kind"),
+            )
+        )
+        .when(expr.struct.field("wrapperType") == "artist")
+        .then(
+            pl.struct(
+                expr.struct.field("artistId").alias("id"),
+                expr.struct.field("artistType").alias("type"),
+                expr.struct.field("artistName").alias("name"),
+                pl.lit(None).alias("kind"),
+            )
+        )
+        .otherwise(
+            pl.struct(
+                expr.struct.field("trackId").alias("id"),
+                pl.lit(None).alias("type"),
+                expr.struct.field("trackName").alias("name"),
+                expr.struct.field("kind").alias("kind"),
+            )
+        )
+    )
---
+++ test_itunes_etl.py
@@ -0,0 +1,108 @@
+# pyright: strict
+
+import polars as pl
+from hypothesis import given, settings
+from hypothesis import strategies as st
+from polars.testing import assert_frame_equal
+
+from itunes_etl import lookup_itunes_id
+
+
+@given(
+    batch_size=st.integers(min_value=1, max_value=15),
+)
+@settings(deadline=None)
+def test_lookup_itunes_id(batch_size: int) -> None:
+    df1 = pl.DataFrame(
+        {
+            "id": [
+                909253,
+                1,
+                1440768692,
+                1440768764,
+                909253,
+                102225079,
+                1438674900,
+                284910350,
+                1676858107,
+                6446905902,
+            ]
+        },
+        schema={"id": pl.UInt64},
+    ).with_columns(
+        pl.col("id").pipe(lookup_itunes_id, batch_size=batch_size).alias("result"),
+    )
+    df2 = pl.DataFrame(
+        {
+            "id": [
+                909253,
+                1,
+                1440768692,
+                1440768764,
+                909253,
+                102225079,
+                1438674900,
+                284910350,
+                1676858107,
+                6446905902,
+            ],
+            "result": [
+                {
+                    "id": 909253,
+                    "type": "Artist",
+                    "name": "Jack Johnson",
+                },
+                None,
+                {
+                    "id": 1440768692,
+                    "type": "Album",
+                    "name": "In Between Dreams",
+                },
+                {
+                    "id": 1440768764,
+                    "kind": "song",
+                    "name": "Banana Pancakes",
+                },
+                {
+                    "id": 909253,
+                    "type": "Artist",
+                    "name": "Jack Johnson",
+                },
+                {
+                    "id": 102225079,
+                    "type": "TV Show",
+                    "name": "The Office",
+                },
+                {
+                    "id": 1438674900,
+                    "type": "TV Season",
+                    "name": "The Office: The Complete Series",
+                },
+                {
+                    "id": 284910350,
+                    "kind": "software",
+                    "name": "Yelp: Food, Delivery & Reviews",
+                },
+                {
+                    "id": 1676858107,
+                    "kind": "feature-movie",
+                    "name": "Avatar: The Way of Water",
+                },
+                {
+                    "id": 6446905902,
+                    "kind": "ebook",
+                    "name": "Make Something Wonderful",
+                },
+            ],
+        },
+        schema={"id": pl.UInt64, "result": df1.schema["result"]},
+    )
+    assert_frame_equal(df1, df2)
+
+
+def test_lookup_itunes_id_empty() -> None:
+    df1 = pl.DataFrame({"id": []}, schema={"id": pl.UInt64}).with_columns(
+        pl.col("id").pipe(lookup_itunes_id).alias("result")
+    )
+    df2 = pl.DataFrame({"id": [], "result": []}, schema=df1.schema)
+    assert_frame_equal(df1, df2)
