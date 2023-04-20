f53f0bcc1fb59f5ec550109e6052f502539b7323
Joshua Peek <josh@users.noreply.github.com>
2023-04-20 20:12:40 +0000

    Use itunes etl lookup

--- appletv.py
+++ appletv.py
@@ -7,10 +7,11 @@ from collections.abc import Iterator
 from typing import Any, Literal, NewType
 
 import backoff
+import polars as pl
 import requests
 from bs4 import BeautifulSoup
 
-import itunes
+from itunes_etl import lookup_itunes_id
 
 session = requests.Session()
 
@@ -50,9 +51,14 @@ def appletv_to_itunes(appletv_id: ID) -> int | None:
     if not possible_itunes_id:
         return None
 
-    for itunes_id, result in itunes.batch_lookup([possible_itunes_id]):
-        if result:
-            return itunes_id
+    itunes_id: int = (
+        pl.Series([possible_itunes_id], dtype=pl.UInt64)
+        .to_frame("id")
+        .select(pl.col("id").pipe(lookup_itunes_id, country="us").struct.field("id"))
+        .item()
+    )
+    if possible_itunes_id == itunes_id:
+        return itunes_id
 
     return None
 
