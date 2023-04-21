8941e85f83f3873516b867a04666c0634a346ac6
Joshua Peek <josh@users.noreply.github.com>
2023-04-21 17:09:40 +0000

    Fix recursive log groups

--- actions.py
+++ actions.py
@@ -1,24 +1,28 @@
 import sys
 import warnings
 from contextlib import contextmanager
-from threading import Lock
+from threading import Lock, local
 from typing import Generator, Type
 from warnings import warn
 
 _GROUP_LOCK = Lock()
+_THREAD_LOCAL = local()
 
 
 @contextmanager
 def log_group(name: str) -> Generator[None, None, None]:
-    if _GROUP_LOCK.locked():
-        raise RuntimeError("Can't nest log groups")
+    if hasattr(_THREAD_LOCAL, "actions_log_group"):
+        current_name: str = _THREAD_LOCAL.actions_log_group
+        raise RuntimeError(f"Can't nest '{name}' log group inside '{current_name}'")
 
     with _GROUP_LOCK:
         try:
+            _THREAD_LOCAL.actions_log_group = name
             print(f"::group::{name}", file=sys.stderr)
             yield
         finally:
             print("::endgroup::", file=sys.stderr)
+            del _THREAD_LOCAL.actions_log_group
 
 
 def print_warning(title: str, message: str) -> None:
