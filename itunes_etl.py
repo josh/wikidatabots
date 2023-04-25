3743cb767c334515ef23fbe61d26c77ec8717a26
Joshua Peek <josh@users.noreply.github.com>
2023-04-25 00:59:22 +0000

    Bump limit

--- itunes_etl.py
+++ itunes_etl.py
@@ -282,7 +282,7 @@ def _discover_ids(df: pl.LazyFrame) -> pl.LazyFrame:
     )
 
 
-_OLDEST_METADATA = pl.col("retrieved_at").rank("ordinal") < (10 * _LOOKUP_BATCH_SIZE)
+_OLDEST_METADATA = pl.col("retrieved_at").rank("ordinal") < (25 * _LOOKUP_BATCH_SIZE)
 _MISSING_METADATA = pl.col("retrieved_at").is_null()
 
 
