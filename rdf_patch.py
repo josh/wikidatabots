c069128d9a2368546711a7ad905cd84302d701b6
Joshua Peek <josh@users.noreply.github.com>
2023-04-30 00:09:18 +0000

    Inline constants into rdf patcher

--- rdf_patch.py
+++ rdf_patch.py
@@ -9,11 +9,12 @@ from typing import Any, Iterator, TextIO
 import pywikibot
 import pywikibot.config
 from rdflib import Graph
+from rdflib.namespace import Namespace, NamespaceManager
 from rdflib.term import BNode, Literal, URIRef
 
 from actions import print_warning
 from pwb import login
-from wikidata import NS_MANAGER, PROV, WIKIBASE, WIKIDATABOTS, page_qids
+from wikidata import page_qids
 
 SITE = pywikibot.Site("wikidata", "wikidata")
 
@@ -30,6 +31,36 @@ class HashableClaim(object):
             return False
         return self.claim == other.claim
 
+
+P = Namespace("http://www.wikidata.org/prop/")
+PQ = Namespace("http://www.wikidata.org/prop/qualifier/")
+PR = Namespace("http://www.wikidata.org/prop/reference/")
+PS = Namespace("http://www.wikidata.org/prop/statement/")
+PSV = Namespace("http://www.wikidata.org/prop/statement/value/")
+PROV = Namespace("http://www.w3.org/ns/prov#")
+WD = Namespace("http://www.wikidata.org/entity/")
+WDREF = Namespace("http://www.wikidata.org/reference/")
+WDS = Namespace("http://www.wikidata.org/entity/statement/")
+WDT = Namespace("http://www.wikidata.org/prop/direct/")
+WDV = Namespace("http://www.wikidata.org/value/")
+WDNO = Namespace("http://www.wikidata.org/prop/novalue/")
+WIKIBASE = Namespace("http://wikiba.se/ontology#")
+
+WIKIDATABOTS = Namespace("https://github.com/josh/wikidatabots#")
+
+NS_MANAGER = NamespaceManager(Graph())
+NS_MANAGER.bind("wikibase", WIKIBASE)
+NS_MANAGER.bind("wd", WD)
+NS_MANAGER.bind("wds", WDS)
+NS_MANAGER.bind("wdv", WDV)
+NS_MANAGER.bind("wdref", WDREF)
+NS_MANAGER.bind("wdt", WDT)
+NS_MANAGER.bind("p", P)
+NS_MANAGER.bind("wdno", WDNO)
+NS_MANAGER.bind("ps", PS)
+NS_MANAGER.bind("psv", PSV)
+NS_MANAGER.bind("pq", PQ)
+NS_MANAGER.bind("pr", PR)
 
 PREFIXES = """
 PREFIX bd: <http://www.bigdata.com/rdf#>
--- wikidata.py
+++ wikidata.py
@@ -3,41 +3,8 @@
 import json
 
 import polars as pl
-from rdflib import Graph
-from rdflib.namespace import Namespace, NamespaceManager
 
 from polars_requests import Session, prepare_request, response_text, urllib3_requests
-
-P = Namespace("http://www.wikidata.org/prop/")
-PQ = Namespace("http://www.wikidata.org/prop/qualifier/")
-PR = Namespace("http://www.wikidata.org/prop/reference/")
-PS = Namespace("http://www.wikidata.org/prop/statement/")
-PSV = Namespace("http://www.wikidata.org/prop/statement/value/")
-PROV = Namespace("http://www.w3.org/ns/prov#")
-WD = Namespace("http://www.wikidata.org/entity/")
-WDREF = Namespace("http://www.wikidata.org/reference/")
-WDS = Namespace("http://www.wikidata.org/entity/statement/")
-WDT = Namespace("http://www.wikidata.org/prop/direct/")
-WDV = Namespace("http://www.wikidata.org/value/")
-WDNO = Namespace("http://www.wikidata.org/prop/novalue/")
-WIKIBASE = Namespace("http://wikiba.se/ontology#")
-
-WIKIDATABOTS = Namespace("https://github.com/josh/wikidatabots#")
-
-NS_MANAGER = NamespaceManager(Graph())
-NS_MANAGER.bind("wikibase", WIKIBASE)
-NS_MANAGER.bind("wd", WD)
-NS_MANAGER.bind("wds", WDS)
-NS_MANAGER.bind("wdv", WDV)
-NS_MANAGER.bind("wdref", WDREF)
-NS_MANAGER.bind("wdt", WDT)
-NS_MANAGER.bind("p", P)
-NS_MANAGER.bind("wdno", WDNO)
-NS_MANAGER.bind("ps", PS)
-NS_MANAGER.bind("psv", PSV)
-NS_MANAGER.bind("pq", PQ)
-NS_MANAGER.bind("pr", PR)
-
 
 _SESSION = Session()
 
