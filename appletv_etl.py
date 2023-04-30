87988b03fae21ac63cbe3ae5d5d1799dcb55e215
Joshua Peek <josh@users.noreply.github.com>
2023-04-30 00:03:11 +0000

    Merge into file

--- appletv_etl.py
+++ appletv_etl.py
@@ -1,14 +1,15 @@
 # pyright: strict
 
 import html
+import json
+import re
 import urllib.parse
 import zlib
-from typing import Literal
+from typing import Any, Literal
 
 import polars as pl
 from bs4 import BeautifulSoup
 
-from appletv import extract_itunes_id
 from polars_requests import (
     Session,
     prepare_request,
@@ -313,6 +314,60 @@ def appletv_to_itunes_series(s: pl.Series) -> pl.Series:
         .to_series()
         .alias(s.name)
     )
+
+
+def _extract_shoebox(soup: BeautifulSoup) -> list[Any]:
+    script = soup.find("script", {"type": "fastboot/shoebox", "id": "shoebox-uts-api"})
+    if not script:
+        return []
+
+    return json.loads(script.text).values()
+
+
+def extract_itunes_id(text: str) -> int | None:
+    soup = BeautifulSoup(text, "html.parser")
+
+    if soup.find("h1", string="This content is no longer available."):
+        return None
+
+    link = soup.find("link", attrs={"rel": "canonical"})
+    if not link:
+        return None
+
+    for data in _extract_shoebox(soup):
+        if "content" in data and "playables" in data["content"]:
+            for playable in data["content"]["playables"]:
+                if playable.get("isItunes", False) is True:
+                    return int(playable["externalId"])
+
+        if "playables" in data:
+            for playable in data["playables"].values():
+                if playable["channelId"] == "tvs.sbd.9001":
+                    return int(playable["externalId"])
+
+        if "howToWatch" in data:
+            for way in data["howToWatch"]:
+                if way["channelId"] != "tvs.sbd.9001":
+                    continue
+
+                if way.get("punchoutUrls"):
+                    m = re.match(
+                        r"itmss://itunes.apple.com/us/[^/]+/[^/]+/id(\d+)",
+                        way["punchoutUrls"]["open"],
+                    )
+                    if m:
+                        return int(m.group(1))
+
+                if way.get("versions"):
+                    for version in way["versions"]:
+                        m = re.match(
+                            r"tvs.sbd.9001:(\d+)",
+                            version["playableId"],
+                        )
+                        if m:
+                            return int(m.group(1))
+
+    return None
 
 
 _REGIONS = ["us", "gb", "au", "br", "de", "ca", "it", "es", "fr", "jp", "cn"]
