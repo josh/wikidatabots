8cf2a4c90d5c9c5b61bad2f7ff1257d5dfdd915a
Joshua Peek <josh@users.noreply.github.com>
2023-05-02 18:18:05 +0000

    Add return types

--- test_rdf_patch.py
+++ test_rdf_patch.py
@@ -8,13 +8,13 @@ from rdf_patch import process_graph
 username = "Test"
 
 
-def setup_function(function):
+def setup_function(function) -> None:
     rdf_patch.get_item_page.cache_clear()
     rdf_patch.get_property_page.cache_clear()
     rdf_patch.resolve_claim_guid.cache_clear()
 
 
-def test_change_statement_rank():
+def test_change_statement_rank() -> None:
     triple = [
         "wds:Q172241-6B571F20-7732-47E1-86B2-1DFA6D0A15F5",
         "wikibase:rank",
@@ -32,7 +32,7 @@ def test_change_statement_rank():
     assert claims[0]["rank"] == "deprecated"
 
 
-def test_noop_change_statement_rank():
+def test_noop_change_statement_rank() -> None:
     triple = [
         "wds:Q172241-6B571F20-7732-47E1-86B2-1DFA6D0A15F5",
         "wikibase:rank",
@@ -43,7 +43,7 @@ def test_noop_change_statement_rank():
     assert len(edits) == 0
 
 
-def test_add_prop_direct_value():
+def test_add_prop_direct_value() -> None:
     triple = [
         "wd:Q172241",
         "wdt:P4947",
@@ -61,20 +61,20 @@ def test_add_prop_direct_value():
     assert claims[0]["mainsnak"]["datavalue"]["value"] == "123"
 
 
-def test_noop_change_prop_direct_value():
+def test_noop_change_prop_direct_value() -> None:
     triple = ["wd:Q172241", "wdt:P4947", '"278"', "."]
     edits = list(process_graph(username, StringIO(" ".join(triple))))
     assert len(edits) == 0
 
 
 # TODO: This should probably add a new statement
-def test_noop_change_prop_direct_deprecated_value():
+def test_noop_change_prop_direct_deprecated_value() -> None:
     triple = ["wd:Q1292541", "wdt:P4947", '"429486"', "."]
     edits = list(process_graph(username, StringIO(" ".join(triple))))
     assert len(edits) == 0
 
 
-def test_add_prop_statement_value():
+def test_add_prop_statement_value() -> None:
     triples = [
         "wd:Q172241",
         "p:P4947",
@@ -95,7 +95,7 @@ def test_add_prop_statement_value():
     assert claims[0]["mainsnak"]["datavalue"]["value"] == "123"
 
 
-def test_add_prop_qualifer():
+def test_add_prop_qualifer() -> None:
     triple = [
         "wds:q172241-E0C7392E-5020-4DC1-8520-EEBF57C3AB66",
         "pq:P4633",
@@ -116,7 +116,7 @@ def test_add_prop_qualifer():
     assert claims[0]["qualifiers"]["P4633"][1]["datavalue"]["value"] == "Narrator"
 
 
-def test_noop_change_prop_qualifer():
+def test_noop_change_prop_qualifer() -> None:
     triple = [
         "wds:q172241-91B6C9F4-2F78-4577-9726-6E9D8D76B486",
         "pq:P4633",
@@ -127,7 +127,7 @@ def test_noop_change_prop_qualifer():
     assert len(edits) == 0
 
 
-def test_delete_prop_qualifer():
+def test_delete_prop_qualifer() -> None:
     triple = [
         "wds:Q1292541-2203A57C-488F-4371-9F88-9A5EB91C4883",
         "pq:P2241",
@@ -144,7 +144,7 @@ def test_delete_prop_qualifer():
     assert claims[0].get("qualifiers") is None
 
 
-def test_noop_change_prop_statement():
+def test_noop_change_prop_statement() -> None:
     triple = [
         "wds:q172241-E0C7392E-5020-4DC1-8520-EEBF57C3AB66",
         "ps:P161",
@@ -155,7 +155,7 @@ def test_noop_change_prop_statement():
     assert len(edits) == 0
 
 
-def test_add_item_prop_qualifer():
+def test_add_item_prop_qualifer() -> None:
     triples = [
         "wd:Q172241",
         "p:P161",
