8638184c30c515c70f208328b5dd875a60f09bf4
Joshua Peek <josh@users.noreply.github.com>
2023-10-11 18:39:00 +0000

    Fix bad import

--- wd_opencritic.py
+++ wd_opencritic.py
@@ -2,7 +2,7 @@
 
 import polars as pl
 
-from polars_utils import enable_string_cache, print_rdf_statements
+from polars_utils import enable_string_cache, print_rdf_statements, scan_s3_parquet_anon
 from sparql import sparql
 from wikidata import is_blocked_item
 
