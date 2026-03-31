779d7531357ae63c3bdf46151f5f287c08a77113
Joshua Peek <josh@users.noreply.github.com>
2026-03-31 12:22:21 -0700

    Remove Plex statements

--- test_wd_plex.py
+++
@@ -1,8 +0,0 @@
-import polars as pl
-
-from wd_plex import find_plex_guids_via_tmdb_id
-
-
-def test_find_plex_guids_via_tmdb_id() -> None:
-    ldf = find_plex_guids_via_tmdb_id()
-    assert ldf.collect_schema() == pl.Schema({"rdf_statement": pl.Utf8})
